0922c7ce158217b3b5026f99350e61a44fb01c8d
Chaeeun Kang <codms7020@naver.com>
2025-03-17 15:59:19 +0900

    [level 2] Title: 타겟 넘버, Time: 5.65 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -3,22 +3,20 @@ import java.util.*;
 class Solution {
     
     private static int len, cnt;
-    private static int[] numArr;
     
     public int solution(int[] numbers, int target) {
         
         cnt = 0;
         len = numbers.length;
-        numArr = numbers;
         
-        dfs(0,numArr[0],target);
-        dfs(0,-numArr[0],target);
+        dfs(0,numbers[0],target,numbers);
+        dfs(0,-numbers[0],target,numbers);
         
         return cnt;
         
     }
     
-    private static void dfs(int dep, int num, int target){
+    private static void dfs(int dep, int num, int target, int [] numbers){
         
         if(dep == len-1 && num == target){
             cnt ++;
@@ -27,8 +25,8 @@ class Solution {
         
         if(dep >= len-1) return;
         
-        dfs(dep+1,num+numArr[dep+1],target);
-        dfs(dep+1,num-numArr[dep+1],target);
+        dfs(dep+1,num+numbers[dep+1],target,numbers);
+        dfs(dep+1,num-numbers[dep+1],target,numbers);
         
     }
 }
