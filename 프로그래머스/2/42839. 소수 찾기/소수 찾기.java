ab79daf3b9256814f9b71d7c97ca987a20d8aa68
Chaeeun Kang <codms7020@naver.com>
2025-03-22 15:16:44 +0900

    [level 2] Title: 소수 찾기, Time: 2.08 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+class Solution {
+    
+    private static int size;
+    private static int cnt;
+    
+    private static int [] numArr;
+    private static boolean [] visited;
+    
+    private static Set<Integer> set;
+    
+    public int solution(String numbers) {
+        
+        size = numbers.length();
+        
+        numArr = new int[size];
+        for(int i = 0; i<size; i++){
+            numArr[i] = numbers.charAt(i) - '0';
+        }
+        
+        cnt = 0;
+        visited = new boolean[size];
+        set = new HashSet<>();
+        dfs(0,0);
+        
+        return cnt;
+        
+    }
+    
+    private static void dfs(int n, int dep){
+        // System.out.println(n);
+        if(dep <= size){
+            if(!set.contains(n)){
+                //소수 판별 
+                if(isPrime(n)) cnt ++;
+                set.add(n);
+            }
+        }
+        
+        if(dep == size) return;
+        
+        for(int i = 0 ; i<size; i++){
+            
+            if(visited[i]) continue;
+            
+            visited[i] = true;
+            dfs(n*10+numArr[i],dep+1);
+            visited[i] = false;  
+            
+        }
+    }
+    
+    private static boolean isPrime(int num){
+        
+        if(num == 0 || num == 1) return false;
+        
+        for(int i = 2; i<=Math.sqrt(num); i++){
+            if(num%i == 0) return false;
+        }
+        
+        return true;
+    }
+}
