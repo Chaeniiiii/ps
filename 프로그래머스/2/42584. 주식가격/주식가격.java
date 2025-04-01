7178d532be1de363bc551a038b2b159a56683c47
Chaeeun Kang <codms7020@naver.com>
2025-04-01 16:45:00 +0900

    [level 2] Title: 주식가격, Time: 75.14 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    
+    private static class Num {
+        
+        int idx;
+        int n;
+        
+        private Num(int idx, int n){
+            this.idx = idx;
+            this.n = n;
+        }
+        
+    }
+    
+    public int[] solution(int[] prices) {
+        
+        Deque<Num> deque = new ArrayDeque<>();
+        
+        int [] result = new int[prices.length];
+        // Arrays.fill(result,1);
+        
+        for(int i = 0; i<prices.length; i++){
+            
+            Num num = new Num(i,prices[i]);
+            int size = deque.size();
+            
+            for(int s = 0 ; s<size; s++){
+                Num next = deque.poll();
+                
+                if(next.n <= num.n) deque.add(next);
+                result[next.idx]++;
+                
+            }
+            
+            deque.add(num);
+            
+        }
+                                                    
+        return result;
+        
+    }
+}
