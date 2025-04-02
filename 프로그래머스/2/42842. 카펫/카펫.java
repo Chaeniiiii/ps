1c40d7ba1995f6cd970e75485d5af6631fcf8460
Chaeeun Kang <codms7020@naver.com>
2025-04-02 10:05:16 +0900

    [level 2] Title: 카펫, Time: 9.87 ms, Memory: 89.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        
+        int total = brown + yellow;
+        
+        int bCol = 1, bRow = total;
+        int yCol = bCol-2, yRow = bRow-2;
+        
+        while(bCol != total){
+            
+            if(bCol * bRow == total) {
+                if(yCol>0 && bRow>0){
+                    if(yCol*yRow == yellow) return new int[]{bRow,bCol};
+                }
+            }
+            
+            bRow --;
+            bCol = total/bRow;
+            
+            yCol = bCol-2;
+            yRow = bRow-2;
+            
+        }
+        
+        return new int[]{bRow,bCol};
+    }
+}
