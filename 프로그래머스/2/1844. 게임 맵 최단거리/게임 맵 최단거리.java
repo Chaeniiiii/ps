cecc378e991798f305eb833502f4e2f7456bb55c
Chaeeun Kang <codms7020@naver.com>
2025-04-24 14:42:25 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.27 ms, Memory: 58.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,38 +1,40 @@
 import java.util.*;
 
 class Solution {
-    
-    private static int col,row;
-    
+
     private static class Pos {
+        
         int x;
         int y;
+        
         private Pos(int x, int y){
             this.x = x;
             this.y = y;
         }
+        
     }
     
     public int solution(int[][] maps) {
-
-        col = maps[0].length;
-        row = maps.length;
-        
         return bfs(maps);
-        
     }
     
-    private static int bfs(int[][] maps){
+    private static int bfs(int [][] maps){
+        
+        int n = maps.length; //row
+        int m = maps[0].length;//col
+        Pos arrive = new Pos(n-1,m-1); //상대팀 진영
+        
+        int [] dx = {-1,1,0,0};
+        int [] dy = {0,0,-1,1};
         
         Deque<Pos> deque = new ArrayDeque<>();
-        deque.add(new Pos(0,0));
+        deque.add(new Pos(0,0)); //시작 위치
         
-        int [] dx = new int[]{-1,1,0,0};
-        int [] dy = new int[]{0,0,-1,1};
-        
-        boolean[][] visited = new boolean[row][col];
+        boolean [][] visited = new boolean[n][m];
+        visited[0][0] = true;
         
         int cnt = 1;
+        
         while(!deque.isEmpty()){
             
             int size = deque.size();
@@ -40,21 +42,20 @@ class Solution {
             for(int i = 0; i<size; i++){
                 
                 Pos mv = deque.poll();
-                if(mv.x == row-1 && mv.y == col-1) return cnt;
+                
+                if(mv.x == arrive.x && mv.y == arrive.y) return cnt;
                 
                 for(int k = 0; k<4; k++){
                     
                     int mvX = mv.x + dx[k];
                     int mvY = mv.y + dy[k];
                     
-                    if(mvX < 0 || mvY < 0 || mvX >= row || mvY >= col || 
-                        maps[mvX][mvY] == 0 || visited[mvX][mvY]) continue;
+                    if(mvX < 0 || mvY < 0 || mvX >= n || mvY >= m || 
+                            visited[mvX][mvY] || maps[mvX][mvY] == 0) continue;
                     
                     visited[mvX][mvY] = true;
                     deque.add(new Pos(mvX,mvY));
-                    
                 }
-                
             }
             
             cnt ++;
