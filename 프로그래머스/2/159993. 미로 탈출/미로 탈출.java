4d8a79331a9038fc1e2bfca130666f6d92430cfa
Chaeeun Kang <codms7020@naver.com>
2025-05-12 15:09:04 +0900

    [level 2] Title: 미로 탈출, Time: 11.02 ms, Memory: 90.8 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,12 +1,19 @@
 import java.util.*;
 
 class Solution {
-
     
+    private static final int SIZE = 100;
+    
+    private static final int [] dx = {-1,1,0,0};
+    private static final int [] dy = {0,0,-1,1};
+    
+    private static int answer;
+    private static int col,row;
     
     private static class Pos {
         int x;
         int y;
+        
         private Pos(int x, int y){
             this.x = x;
             this.y = y;
@@ -15,92 +22,67 @@ class Solution {
     
     public int solution(String[] maps) {
         
-        Deque<Pos> deque = new ArrayDeque<>();
-        Pos start = new Pos(-1,-1);
-        Pos end = new Pos(-1,-1);
-        Pos laber = new Pos(-1,-1);
-    
-        for(int i = 0; i<maps.length; i ++){
-            for(int j = 0; j<maps[i].length(); j++){
+        row = maps.length;
+        col = maps[0].length();
+        
+        Pos st = new Pos(-1,-1);
+        Pos en = new Pos(-1,-1);
+        Pos lb = new Pos(-1,-1);
+        
+        for(int i = 0; i<row; i++){
+            for(int j = 0; j<col; j++){
                 char c = maps[i].charAt(j);
-                switch(c){
-                    case 'S' :
-                        start = new Pos(i,j);
-                        break;
-                    case 'E' :
-                        end = new Pos(i,j);
-                        break;
-                    case 'L' :
-                        laber = new Pos(i,j);
-                        break;
-                }
+                if(c == 'S') st = new Pos(i,j);
+                else if(c == 'E') en = new Pos(i,j);
+                else if(c == 'L') lb = new Pos(i,j);
             }
         }
         
-        //start -> laber
+        answer = 0;
         
-        int cnt = 0;
-            
-        int firstStep = bfs(start,laber,maps) ;
-        if(firstStep == -1) return -1;
+        if(!bfs(st,lb,maps)) return -1;
+        if(!bfs(lb,en,maps)) return -1;
         
-        cnt+=firstStep;
-        
-        //laber -> end
-        int scdStep = bfs(laber,end,maps) ;
-        if(scdStep == -1) return -1;
-        
-        cnt+=scdStep;
-        
-        return cnt;
+        return answer;
         
     }
     
-    private static int bfs(Pos go, Pos arrive, String [] map){
-        
-        int row = map.length;
-        int col = map[0].length();
+    private static boolean bfs(Pos a, Pos b, String [] maps){
         
         Deque<Pos> deque = new ArrayDeque<>();
-        deque.add(go);
+        deque.add(a);
         
         boolean [][] visited = new boolean[row][col];
-        int [] dx = {-1,1,0,0};
-        int [] dy = {0,0,-1,1};
-        
-        int t = 0;
         
         while(!deque.isEmpty()){
             
             int size = deque.size();
+            if(answer > SIZE * SIZE) return false;
             
-            for(int i = 0; i<size; i++){
+            for(int t = 0; t<size; t++){
                 
                 Pos mv = deque.poll();
-                if(mv.x == arrive.x && mv.y == arrive.y) return t;
+                if(mv.x == b.x && mv.y == b.y) return true;
                 
-                for(int k = 0; k<4; k++){
+                for(int i = 0; i<4; i++){
+
+                    int mvX = mv.x + dx[i];
+                    int mvY = mv.y + dy[i];
+
+                    if(mvX < 0 || mvY < 0 || mvX >= row || mvY >= col || visited[mvX][mvY] ||
+                        maps[mvX].charAt(mvY) == 'X') continue;
                     
-                    int mvX = mv.x + dx[k];
-                    int mvY = mv.y + dy[k];
-                    
-                    if(mvX <0 || mvY < 0 || mvX >= row || mvY >= col || visited[mvX][mvY] 
-                                || map[mvX].charAt(mvY) == 'X') continue;
-                    
-                    visited[mvX][mvY] = true;
                     deque.add(new Pos(mvX,mvY));
+                    visited[mvX][mvY] = true;
                     
                 }
-                
             }
             
-            t++;
+            answer ++;
             
         }
         
-        return -1;
+        return false;
         
     }
-    
-    
 }
