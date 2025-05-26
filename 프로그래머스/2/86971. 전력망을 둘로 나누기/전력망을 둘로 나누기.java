9901e3ceb92650c389c4dce09978f0298d335697
Chaeeun Kang <codms7020@naver.com>
2025-05-26 17:09:30 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 6.00 ms, Memory: 92.8 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -2,61 +2,63 @@ import java.util.*;
 
 class Solution {
     
-    private static int [] parent;
-    
+    private static int [] parent ;
+
     private static void union(int x, int y){
-        
         x = find(x);
         y = find(y);
         
         if(x != y){
-            if(x < y) parent[y] = x;
-            else parent[x] = y;
+            if(x > y){
+                parent[x] = y;
+            }
+            else{
+                parent[y] = x;
+            }
         }
-        
     }
     
-    private static int find(int x) {
-        
-        if(parent[x] == x) return x;
-        return parent[x] = find(parent[x]);
-        
+    private static int find(int x){
+        if(x != parent[x]) {
+            return parent[x] = find(parent[x]);
+        }
+        return parent[x];
     }
     
     public int solution(int n, int[][] wires) {
         
+        int result = Integer.MAX_VALUE;
         int idx = 0;
-        int result = Integer.MAX_VALUE ;
         
-        while(idx < n){
+        while(idx < wires.length){
             
             parent = new int[n+1];
-            Map<Integer,Integer> map = new HashMap<>();
-
-            //부모 초기화 
-            for(int i = 1; i<=n; i++) parent[i] = i;
-           
-            for(int i = 0; i< wires.length; i++){
-               if(idx == i) continue;
-               union(wires[i][0],wires[i][1]);
-           }   
-            
-            
-            for(int i = 1; i<=n; i ++){
-                int key = find(i);
-                map.put(key, map.getOrDefault(key,0)+1);
-            }        
-        
-            int cnt = 0;
-            for(int key : map.keySet()) {
-                cnt = Math.abs(cnt-map.get(key));
+            for(int i = 1; i<=n; i++) {
+                parent[i] = i;
             }
             
-            result = Math.min(result,cnt);
-            idx ++ ;
+            for(int i = 0; i<wires.length; i++){
+                if(i == idx) {
+                    continue;
+                }
+                union(wires[i][0],wires[i][1]);
+            }
+
+            Map<Integer,Integer> map = new HashMap<>();
+            for(int i = 1; i<=n; i++){
+                parent[i] = find(i);
+                map.put(parent[i],map.getOrDefault(parent[i],0)+1);
+            }
+            
+            int cnt = 0;
+            for(int key : map.keySet()){
+                cnt = Math.abs(cnt - map.get(key));
+            }
+
+            result = Math.min(cnt,result);
+            idx ++;
             
         }
-        
         
         return result;
         
