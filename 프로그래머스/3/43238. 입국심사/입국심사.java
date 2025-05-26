2757303d5f9b52a1e0a99557975e083dcd231f76
Chaeeun Kang <codms7020@naver.com>
2025-05-26 22:43:57 +0900

    [level 3] Title: 입국심사, Time: 110.78 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    
+    private static final long SIZE = 1000000000;
+    
+    public long solution(int n, int[] times) {
+
+        Arrays.sort(times);
+        long min = 1, max = SIZE*SIZE;
+        
+        long cnt = max;
+        
+        while(min <= max){
+            
+            long mid = (min+max)/2;
+            
+            if(check(mid,times) >= n){
+                max = mid-1;
+                cnt = Math.min(mid,cnt);
+            }
+            else{
+                min = mid+1;
+            }
+        }
+        
+        return cnt;
+        
+    }
+    
+    private static long check(long mid, int [] times){
+        
+        long sum = 0;
+        
+        for(int t : times){
+            sum += mid/t;
+        }
+        
+        return sum;
+        
+    }
+}
