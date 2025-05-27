2b763c0062041430c21ca5cc712d735daf418a3a
Chaeeun Kang <codms7020@naver.com>
2025-05-27 20:55:29 +0900

    [level 1] Title: 신규 아이디 추천, Time: 13.98 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,67 +1,82 @@
 class Solution {
-
-    private static class Kakao {
+    
+    private static class NewId {
+        
         String id;
-
-        private Kakao(String id) {
+        
+        private NewId(String id){
             this.id = id;
         }
 
-        private Kakao replaceSmall() {
-            this.id = this.id.toLowerCase();
+        //1단계                
+        private NewId replaceSmall(){
+            this.id = id.toLowerCase();
             return this;
         }
-
-        private Kakao removeStr() {
-            this.id = this.id.replaceAll("[^a-z0-9._-]", "");
+        
+        //2단계
+        private NewId replaceStr(){
+            this.id = id.replaceAll("[^a-z0-9\\._-]","");
             return this;
         }
-
-        private Kakao replaceDot() {
-            this.id = this.id.replaceAll("\\.{2,}", ".");
+        
+        //3-4단계
+        private NewId dot(){
+            //3단계
+            this.id = id.replaceAll("[.]{2,}",".");
+            //4단계
+            this.id = this.id.replaceAll("^\\.|\\.$", "");
+            
             return this;
         }
-
-        private Kakao removeDot() {
-            if (!this.id.isEmpty() && this.id.charAt(0) == '.')
-                this.id = this.id.substring(1);
-            if (!this.id.isEmpty() && this.id.charAt(this.id.length() - 1) == '.')
-                this.id = this.id.substring(0, this.id.length() - 1);
+        
+        //5단계
+        private NewId putA(){
+            if(this.id.equals("")){
+                this.id+="a";
+            }
             return this;
         }
-
-        private Kakao isNull() {
-            if (this.id.isEmpty()) this.id = "a";
+        
+        //6단계
+        private NewId cutStr(){
+            if(this.id.length() > 15) {
+                this.id = id.substring(0,15);
+            }
             return this;
         }
-
-        private Kakao cutLen() {
-            if (this.id.length() >= 16)
-                this.id = this.id.substring(0, 15);
+        
+        //7단계
+        private NewId attachChar(){
+            
+            while(this.id.length() < 3){
+                
+                this.id += this.id.charAt(this.id.length()-1);
+                
+            }
+            
             return this;
+            
         }
-
-        private Kakao checkLen() {
-            while (this.id.length() < 3)
-                this.id += this.id.charAt(this.id.length() - 1);
-            return this;
+        
+        private String getString(){
+            return this.id.toString();
         }
-
-        private String get() {
-            return this.id;
-        }
+        
     }
-
+    
     public String solution(String new_id) {
-        return new Kakao(new_id)
-                .replaceSmall()
-                .removeStr()
-                .replaceDot()
-                .removeDot()
-                .isNull()
-                .cutLen()
-                .removeDot()
-                .checkLen()
-                .get();  
+        
+        String id = new NewId(new_id)
+            .replaceSmall()
+            .replaceStr()
+            .dot()
+            .putA()
+            .cutStr()
+            .dot()
+            .attachChar()
+            .getString();
+        
+        return id;
     }
 }
