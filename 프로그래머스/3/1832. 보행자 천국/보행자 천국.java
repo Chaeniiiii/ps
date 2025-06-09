115e8bd9f856048453ca9b984cc4a885cdbf464d
Chaeeun Kang <codms7020@naver.com>
2025-06-09 16:27:39 +0900

    [level 3] Title: 보행자 천국, Time: 76.54 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
+++ 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
@@ -1,43 +1,34 @@
 class Solution {
+    
+    private static final int MOD = 20170805;
+    
     public int solution(int m, int n, int[][] cityMap) {
-        int MOD = 20170805;
-        int[][][] dp = new int[m][n][2];
-
-        // 시작점 초기화
-        dp[0][0][0] = dp[0][0][1] = 1;
-
-        // 첫 열: 아래 방향만 존재
-        for (int i = 1; i < m; i++) {
-            if (cityMap[i][0] == 1) break; // 통제 지점 지나면 그 아래 모두 막힘
-            dp[i][0][0] = dp[i - 1][0][0];
-        }
-        // 첫 행: 오른쪽 방향만 존재
-        for (int j = 1; j < n; j++) {
-            if (cityMap[0][j] == 1) break;
-            dp[0][j][1] = dp[0][j - 1][1];
-        }
-
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (cityMap[i][j] == 1) continue; // 통행 금지
-
-                // 위에서 아래(도)
-                if (cityMap[i - 1][j] == 0) {
-                    dp[i][j][0] = (dp[i - 1][j][0] + dp[i - 1][j][1]) % MOD;
-                } else if (cityMap[i - 1][j] == 2) {
-                    dp[i][j][0] = dp[i - 1][j][0];
+        
+        int answer = 0;
+        int[][][] dp = new int[2][m+1][n+1];
+        
+        dp[0][0][0]=1;
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(cityMap[i][j]==0){
+                    //가로
+                    dp[0][i+1][j]+=(dp[0][i][j]+dp[1][i][j])%MOD;
+                    //세로
+                    dp[1][i][j+1]+=(dp[0][i][j]+dp[1][i][j])%MOD;
                 }
-                // 왼쪽에서 오른쪽(우)
-                if (cityMap[i][j - 1] == 0) {
-                    dp[i][j][1] = (dp[i][j - 1][0] + dp[i][j - 1][1]) % MOD;
-                } else if (cityMap[i][j - 1] == 2) {
-                    dp[i][j][1] = dp[i][j - 1][1];
+                else if(cityMap[i][j]==2){
+                    //가로
+                    dp[0][i+1][j]+=dp[0][i][j];
+                    dp[0][i+1][j]%=MOD;
+                    //세로
+                    dp[1][i][j+1]+=dp[1][i][j];
+                    dp[1][i][j+1]%=MOD;
                 }
             }
         }
-
-        return (dp[m - 1][n - 1][0] + dp[m - 1][n - 1][1]) % MOD;
+        
+        return (dp[0][m-1][n-1]+dp[1][m-1][n-1])%MOD;
+        
     }
-
-
 }
