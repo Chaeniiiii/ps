30941e087d17394e4722256413fd40f2f1cca7b1
Chaeeun Kang <codms7020@naver.com>
2025-06-26 17:32:36 +0900

    [level 3] Title: 광고 삽입, Time: 338.22 ms, Memory: 336 MB -BaekjoonHub

--- 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
@@ -2,74 +2,55 @@ import java.util.*;
 
 class Solution {
     
-    private static final int MAX = 360000;
+    long[] times; 
     
     public String solution(String play_time, String adv_time, String[] logs) {
-        
-        int[] time = new int[MAX+1];
+        int adTime = toSeconds(adv_time);
+        int playTime = toSeconds(play_time);
+        times = new long[playTime+1];
+        Arrays.fill(times,0);
         
         for(String log : logs){
-            String[] info = log.split("-");
-            int start = ToInt(info[0]);
-            int end = ToInt(info[1]);
-            
-            time[start]++;
-            time[end]--;
+            String[] splited = log.split("-");
+            times[toSeconds(splited[0])]++;
+            times[toSeconds(splited[1])]--;
+        }
+
+        for(int i = 1; i <= playTime ; i++){
+            times[i] += times[i-1];
         }
         
-        int end = ToInt(play_time);
-        int adv = ToInt(adv_time);
-        int max_start = end - adv;
-        
-        for(int i = 1; i < time.length; i++){
-            time[i]+=time[i-1];
+        for(int i = 1; i <= playTime; i++){
+            times[i] += times[i-1];
         }
         
-        long max = 0;
-        long now = max;
-        int start = 0;
+        int sTime = 0; 
+        long ansTime = times[adTime-1];
         
-        for(int i = adv; i <= end; i++){
-            
-            now += time[i] - time[i-adv];
-            
-            if(now > max){
-                start = i - adv + 1;
-                max = now;
+        for(int i = 1; i <= playTime-adTime;i++){
+            if(times[i+adTime-1] - times[i-1] > ansTime){
+                ansTime = times[i+adTime-1] - times[i-1];
+                sTime = i;
             }
         }
         
-        return ToTime(start);
-        
+        return toFormatted(sTime);
     }
     
-    public int ToInt(String time){
+    public int toSeconds(String time){
+        String[] splited = time.split(":");
+        int hours = Integer.parseInt(splited[0]);
+        int minutes = Integer.parseInt(splited[1]);
+        int seconds = Integer.parseInt(splited[2]);
         
-        String[] t = time.split(":");
-        
-        return Integer.parseInt(t[2])+
-            Integer.parseInt(t[1])*60+
-            Integer.parseInt(t[0])*60*60;
+        return hours * 3600 + minutes * 60 + seconds;
     }
     
-    public String ToTime(int time){
+    public String toFormatted(int time){
+        int hours = time / 3600;
+        int minutes = (time % 3600) / 60;
+        int seconds = (time % 3600) % 60;
         
-        int hour= time/3600;
-        String sh=String.valueOf(hour);
-        if(hour<10) sh="0"+sh;
-                
-        time-=hour*3600;
-        
-        int min= time/60;
-        time-=min*60;
-        String mh=String.valueOf(min);
-        
-        if(min<10) mh="0"+mh;
-        
-        String ch=String.valueOf(time);
-        if(time<10) ch="0"+ch;
-        
-        return sh+":"+mh+":"+ch;
-        
+        return String.format("%02d:%02d:%02d",hours,minutes,seconds);
     }
 }
