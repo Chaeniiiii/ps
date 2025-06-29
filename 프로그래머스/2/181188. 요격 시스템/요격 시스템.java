b60903d914dd3f6da0027954a0432772f9b15e74
Chaeeun Kang <codms7020@naver.com>
2025-06-29 21:23:35 +0900

    [level 2] Title: 요격 시스템, Time: 413.75 ms, Memory: 184 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -2,17 +2,20 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] targets) {
-
-        Arrays.sort(targets, (a,b) -> a[1]-b[1]);
+        
+        Arrays.sort(targets,(a,b) -> a[1] - b[1]);
         
         int end = targets[0][1];
         int cnt = 1;
         
-        for(int i = 1; i<targets.length; i++){
-            if(targets[i][0] >= end){
+        for(int i = 1; i < targets.length; i++){
+            
+            int[] nxt = targets[i];
+            if(end <= nxt[0]){
+                end = nxt[1];
                 cnt ++;
-                end = targets[i][1];
             }
+            
         }
         
         return cnt;
