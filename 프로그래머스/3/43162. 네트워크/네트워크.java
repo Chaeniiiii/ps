b62a55172041e684e21b179459fc121b3c84a1ad
Chaeeun Kang <codms7020@naver.com>
2025-07-14 21:34:39 +0900

    [level 3] Title: 네트워크, Time: 0.40 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -2,48 +2,55 @@ import java.util.*;
 
 class Solution {
     
-    private static int [] parent;
+    private static int[] parent;
+    private static Set<Integer> network;
     
     private static void union(int x, int y){
         
         x = find(x);
         y = find(y);
         
-        if(x != y){
-            if(x < y) parent[y] = x;
-            else parent[x] = y;
+        if(x < y){
+            parent[y] = x;
+        }
+        else{
+            parent[x] = y;
         }
         
     }
     
     private static int find(int x){
         
-        if(parent[x] == x) return parent[x];
-        
-        return parent[x] = find(parent[x]);
+        if(x == parent[x]) return parent[x];
+        return find(parent[x]);
         
     }
     
     public int solution(int n, int[][] computers) {
         
-        Set<Integer> set = new HashSet<>();
-        parent = new int[n+1];
+        network = new HashSet<>();
+        parent = new int[n];
+        for(int i = 0; i < n; i++){
+            parent[i] = i;
+        }
         
-        //부모 초기화
-        for(int i = 1; i<=n ; i++) parent[i] = i;
-        
-        //부모 갱신
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<n; j++){
-                if(computers[i][j] == 1) union(i+1,j+1);
+        for(int i = 0 ; i < n; i++){
+            for(int j = 0 ; j < n; j++){
+                if(computers[i][j] == 0) continue;
+                union(i, j);    
             }
         }
         
-        for(int i = 1; i<=n; i++){
-            set.add(find(i));
+        for(int i = 0; i < n; i++){
+            parent[i] = find(i);
         }
         
-        return set.size();
+        
+        for(int i = 0; i < n; i++){
+            network.add(parent[i]);
+        }
+        
+        return network.size();
         
     }
 }
