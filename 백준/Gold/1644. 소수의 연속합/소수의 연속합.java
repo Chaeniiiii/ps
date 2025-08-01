60d62bf7455ce71d6038cef12fc44d3cbf99e3e9
Chaeeun Kang <codms7020@naver.com>
2025-08-01 23:10:27 +0900

    [Gold III] Title: 소수의 연속합, Time: 184 ms, Memory: 26856 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -3,15 +3,14 @@ import java.io.*;
 
 public class Main{
 
+    private static ArrayList<Integer> arr;
     public static void main(String [] args) throws Exception{
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        ArrayList<Integer> arr = new ArrayList<>();
-        for(int i = 2; i <= n; i++){
-            if(isPrime(i)) arr.add(i);
-        }
+        arr = new ArrayList<>();
+        isPrime(n);
 
         int lt = 0, rt = lt , cnt = 0, result = 0;
 
@@ -34,10 +33,16 @@ public class Main{
         
     }
 
-    private static boolean isPrime(int num){
-        for(int i = 2; i <= Math.sqrt(num); i++){
-            if(num % i == 0) return false;
+    private static void isPrime(int n){
+        
+        boolean[] visited = new boolean[n+1];
+        
+        for(int i = 2; i <= n; i++){
+            if(visited[i]) continue;
+            arr.add(i);
+            for(int j = i * 2; j <= n; j+=i){
+                visited[j] = true;
+            }
         }
-        return true;
     }
 }
