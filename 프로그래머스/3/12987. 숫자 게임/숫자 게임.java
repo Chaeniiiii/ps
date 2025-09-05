c4cc74800dec9e571e9385d32bb7961f0edcd5ac
Chaeeun Kang <codms7020@naver.com>
2025-09-05 11:30:09 +0900

    [level 3] Title: 숫자 게임, Time: 61.62 ms, Memory: 70.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        int aIdx = 0, bIdx = 0, result = 0;
+        
+        while(aIdx < A.length && bIdx < B.length){
+            if(A[aIdx] < B[bIdx]){
+                aIdx++;
+                bIdx++;
+                result++;
+            }
+            else{
+                bIdx++;
+            }
+        }
+        
+        return result;
+        
+    }
+}
