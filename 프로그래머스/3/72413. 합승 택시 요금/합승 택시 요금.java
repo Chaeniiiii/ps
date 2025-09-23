a4f913ef166364b35dffc3fbe2870f3aa0618c8f
Chaeeun Kang <codms7020@naver.com>
2025-09-23 23:31:14 +0900

    [level 3] Title: 합승 택시 요금, Time: 239.99 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -2,84 +2,80 @@ import java.util.*;
 
 class Solution {
     
-    private static final int INF = Integer.MAX_VALUE;
+    private static final int MAX = Integer.MAX_VALUE;
     
     private static class Node{
-        int nxt;
-        int cost;
+        int num;
+        int dist;
         
-        private Node(int nxt, int cost){
-            this.nxt = nxt;
-            this.cost = cost;
+        private Node(int num, int dist){
+            this.num = num;
+            this.dist = dist;
         }
     }
     
     public int solution(int n, int s, int a, int b, int[][] fares) {
         
-        Map<Integer,ArrayList<Node>> graph = new HashMap<>();
+        int[][] graph = new int[n+1][n+1];
         
-        for(int i = 1; i <= n; i++){
-            graph.put(i, new ArrayList<>());
-        }
-        
+        //그래프 구성
         for(int[] fare : fares){
             int v = fare[0];
             int u = fare[1];
-            int t = fare[2];
+            int d = fare[2];
             
-            graph.get(v).add(new Node(u,t));
-            graph.get(u).add(new Node(v,t));
-            
+            graph[v][u] = d;
+            graph[u][v] = d;
         }
         
-        int[] startDist = dijkstra(n,s,graph);
-        int[] aDist = dijkstra(n,a,graph);
-        int[] bDist = dijkstra(n,b,graph);
+        int[] tDist = dijkstra(n,graph,s);
+        int total = MAX;
         
-        int result = INF;
-
-        for(int i = 1; i <=n; i++){
-            
-            int cnt = startDist[i] + aDist[i] + bDist[i];
-            
-            if(result > cnt){
-                result = cnt;
+        for(int i = 1; i <= n ; i++){
+            int[] alone = dijkstra(n,graph,i);
+            int cnt = tDist[i] + alone[a] + alone[b];
+            if(total > cnt){
+                total = cnt;
             }
-            
         }
         
-        return result;
+        return total;
         
     }
     
-    private static int[] dijkstra(int n, int st, Map<Integer,ArrayList<Node>> graph){
+    private int[] dijkstra(int n, int[][] graph, int st){
         
-        PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a.cost));
+        PriorityQueue<Node> pq= new PriorityQueue<>(Comparator.comparingInt(a -> a.dist));
         pq.add(new Node(st,0));
         
+        boolean[] visited = new boolean[n+1];
         int[] dist = new int[n+1];
-        Arrays.fill(dist,INF);
+        
+        Arrays.fill(dist,MAX);
         dist[st] = 0;
         
         while(!pq.isEmpty()){
             
             Node now = pq.poll();
             
-            for(Node node : graph.get(now.nxt)){
-                
-                int newCost = node.cost + dist[now.nxt];
-                
-                if(dist[node.nxt] > newCost){
-                    dist[node.nxt] = newCost;
-                    pq.add(new Node(node.nxt,dist[node.nxt]));
-                }
-                
+            if(visited[now.num]){
+                continue;
             }
             
-        }
+            visited[now.num] = true;
+            
+            for(int i = 1; i <= n; i++){
+                if(graph[now.num][i] == 0){
+                    continue;
+                }
+                if(dist[i] > graph[now.num][i] + dist[now.num]){
+                    dist[i] = graph[now.num][i] + dist[now.num];
+                    pq.add(new Node(i,dist[i]));
+                }
+            }
+        }        
         
         return dist;
         
     }
-    
 }
