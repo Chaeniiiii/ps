f6b6c9adaf03cb5fb94e19643a6766e7db578cf0
Chaeeun Kang <codms7020@naver.com>
2025-10-19 23:59:44 +0900

    [level 3] Title: 등대, Time: 114.63 ms, Memory: 138 MB -BaekjoonHub

--- 프로그래머스/3/133500. 등대/등대.java
+++ 프로그래머스/3/133500. 등대/등대.java
@@ -2,45 +2,49 @@ import java.util.*;
 
 class Solution {
     
+    private static Map<Integer,ArrayList<Integer>> map;
+    
+    private static boolean[] visited;
     private static int[][] dp;
-    private static Map<Integer,ArrayList<Integer>> graph;
     
     public int solution(int n, int[][] lighthouse) {
         
-        graph = new HashMap<>();
+        map = new HashMap<>();
+        
+        visited = new boolean[n+1];
         dp = new int[n+1][2];
         
         for(int i = 1; i <= n; i++){
-            graph.put(i, new ArrayList<>());
+            map.put(i, new ArrayList<>());
         }
         
-        for(int[] light : lighthouse){
-            graph.get(light[0]).add(light[1]);
-            graph.get(light[1]).add(light[0]);
+        for(int i = 0; i < lighthouse.length; i++){
+            int[] now = lighthouse[i];
+            map.get(now[0]).add(now[1]);
+            map.get(now[1]).add(now[0]);
         }
-
-        dfs(1,0);
         
-        return Math.min(dp[1][0], dp[1][1]);
+        visited[1] = true;
+        dfs(1);
         
+        return Math.min(dp[1][0],dp[1][1]);
+         
     }
     
-    private static void dfs(int st, int parent){
+    private static void dfs(int st){
         
-        dp[st][0] = 0;
-        dp[st][1] = 1;
+        dp[st][1] = 1; //자기 자신을 무조건 켜야할 때
         
-        for(int child : graph.get(st)){
+        for(int child : map.get(st)){
+            if(visited[child]) continue;
+            visited[child] = true;
             
-            if(child == parent) continue;
-            
-            dfs(child,st);
-            
+            dfs(child);
             dp[st][0] += dp[child][1];
-            dp[st][1] += Math.min(dp[child][0], dp[child][1]);
-            
+            dp[st][1] += Math.min(dp[child][0],dp[child][1]);
         }
         
+                
     }
     
 }
