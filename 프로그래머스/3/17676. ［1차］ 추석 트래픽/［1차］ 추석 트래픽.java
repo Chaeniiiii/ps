b4b06bf388d90e6c20af472c5c1159dd95e1623c
Chaeeun Kang <codms7020@naver.com>
2025-10-21 16:24:51 +0900

    [level 3] Title: [1차] 추석 트래픽, Time: 50.71 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.java
+++ 프로그래머스/3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.java
@@ -2,70 +2,63 @@ import java.util.*;
 
 class Solution {
     
-    private static class Traffic {
-        int start;
-        int end;
+    private static class Task{
         
-        private Traffic(int start, int end) {
-            this.start = start;
-            this.end = end;
-        }
+        int st;
+        int en;
+        
+        private Task(int st, int en){
+            this.st = st;
+            this.en = en;
+        }  
     }
     
     public int solution(String[] lines) {
         
-        ArrayList<Traffic> arr = new ArrayList<>();
+        ArrayList<Task> arr = new ArrayList<>();
         
-        for(int i = 0; i < lines.length; i++) {
-            String[] line = lines[i].replace("2016-09-15 ", "").split(" ");
-            String[] endTime = line[0].split(":");
+        for(String line : lines){
             
-            // 끝나는 시간을 밀리초로 변환
-            int end = getTime(endTime);
+            String[] taskT = line.split(" ");
+            String[] en = taskT[1].split(":");
             
-            // 처리 시간을 밀리초로 변환
-            int duration = (int)(Double.parseDouble(line[1].replace("s", "")) * 1000);
+            double s = Double.parseDouble(en[2]) - Double.parseDouble(taskT[2].replace("s",""));
             
-            // 시작 시간 = 끝 시간 - 처리 시간 + 1
-            int start = end - duration + 1;
+            String[] st = en.clone();
+            st[2] = String.valueOf(s);
             
-            arr.add(new Traffic(start, end));
+            int stT = convert(st) + 1;
+            int enT = convert(en);
+            
+            Task task = new Task(stT,enT);
+            arr.add(task);        
+            
         }
         
-        int result = 1;
-        
-        for(Traffic trf : arr) {
-            // 각 로그의 시작 시점에서 1초 구간 검사
-            int startCnt = getMaxCount(arr, trf.start);
-            // 각 로그의 끝 시점에서 1초 구간 검사  
-            int endCnt = getMaxCount(arr, trf.end);
-            
-            result = Math.max(result, Math.max(startCnt, endCnt));
+        int result = 0;
+        for(int i = 0; i < arr.size(); i++){
+            int cnt = 0;
+            Task now = arr.get(i);
+            for(int j = i; j < arr.size(); j ++){
+                Task nxt = arr.get(j);
+                if(now.en + 1000 - 1 < nxt.st) continue;
+                cnt++;
+            }
+            result = Math.max(result,cnt);
         }
         
         return result;
+        
     }
     
-    private int getMaxCount(ArrayList<Traffic> arr, int startTime) {
+    private static int convert(String[] time){
         
-        int cnt = 0;
+        int h = Integer.parseInt(time[0])* 1000;
+        int m = Integer.parseInt(time[1]) * 1000;
+        int s = (int)(Double.parseDouble(time[2]) * 1000.0);
         
-        for(Traffic trf : arr) {
-            if(trf.start < startTime + 1000 && trf.end >= startTime) {
-                 cnt++;
-            }
-        }
+        return h*3600 + m*60 + s;
         
-        return cnt;
     }
     
-    private static int getTime(String[] time) {
-        
-        int h = Integer.parseInt(time[0]) * 3600 * 1000;
-        int m = Integer.parseInt(time[1]) * 60 * 1000;
-        int ms = (int)(Double.parseDouble(time[2]) * 1000);
-        
-        return h + m + ms;
-        
-    }
 }
