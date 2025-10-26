ebc851c709c04ca351920d297f87068c349dec23
Chaeeun Kang <codms7020@naver.com>
2025-10-26 14:33:50 +0900

    [level 2] Title: 비밀 코드 해독, Time: 11.63 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
@@ -2,60 +2,51 @@ import java.util.*;
 
 class Solution {
     
-    private static final int MAX = 5;
+    private static final int SIZE = 5;
+    private static int result;
     
-    private static int n,result;
     private static int[][] q;
     private static int[] ans;
-    
-    public int solution(int n, int[][] q, int[] ans) {
 
-        this.n = n;
+    public int solution(int n, int[][] q, int[] ans) {
+        
         this.q = q;
         this.ans = ans;
         
         result = 0;
-        dfs(1,0,new boolean[n+1]);
+        dfs(1,n,0, new boolean[n+1]);
         
         return result;
         
     }
     
-    private static void dfs(int num, int dep, boolean[] arr){
+    private static void dfs(int idx, int n, int dep, boolean[] arr){
         
-        if(dep == MAX){
-            if(isPossible(arr)) result ++;
+        if(dep == SIZE ){
+            if(calc(arr)) result++;
             return;
         }
         
-        for(int i = num; i <= n; i++){
-            
+        for(int i = idx; i <= n; i++){
             arr[i] = true;
-            dfs(i+1,dep+1,arr);
+            dfs(i+1,n,dep+1,arr);
             arr[i] = false;
-            
-        }        
+        }
         
     }
     
-    private static boolean isPossible(boolean[] arr){
+    private static boolean calc(boolean[] arr){
         
         for(int i = 0; i < q.length; i++){
-            
+            int[] query = q[i];
             int cnt = 0;
-            int[] qArr = q[i];
-            
-            for(int ele : qArr){
-                if(arr[ele]) cnt++;
-                if(cnt > ans[i]) return false;
+            for(int j = 0; j < SIZE; j++){
+                if(arr[query[j]]) cnt++;
             }
-            
             if(cnt != ans[i]) return false;
-            
         }
         
         return true;
-        
         
     }
     
