f8b8c97a604e6b6c21bbb8629266148b6aaf2cd2
Chaeeun Kang <codms7020@naver.com>
2025-10-29 10:56:00 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,5 +1,2 @@
-select ai.name, ai.datetime
-from animal_ins ai left join animal_outs ao on ai.animal_id = ao.animal_id 
-where SEX_UPON_OUTCOME is null
-order by datetime
-limit 3;
+-- 코드를 입력하세요
+SELECT
