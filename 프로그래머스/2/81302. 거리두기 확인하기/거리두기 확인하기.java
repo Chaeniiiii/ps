53e48a8b3a954229f12e1cea4baa2308dcf7824e
Chaeeun Kang <codms7020@naver.com>
2025-11-08 17:24:35 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.08 ms, Memory: 92.3 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -4,83 +4,76 @@ class Solution {
     
     private static final int SIZE = 5;
     
-    private static final int [] dx = {-1,1,0,0};
-    private static final int [] dy = {0,0,-1,1};
+    private static int[] dx = new int[]{-1,1,0,0};
+    private static int[] dy = new int[]{0,0,-1,1};
     
-    private static boolean [][] visited;
-    
-    private static class Pos {
-        
+    private static int[] daX = new int[]{1,1,-1,-1};
+    private static int[] daY = new int[]{1,-1,1,-1};
+
+    private static class Pos{
         int x;
         int y;
-        
         private Pos(int x, int y){
             this.x = x;
             this.y = y;
         }
-        
     }
     
     public int[] solution(String[][] places) {
         
-        int tc = places.length;
-        int [] result = new int[tc];
+        int[] result = new int[SIZE];
+        Arrays.fill(result,1);
         
-        for(int t = 0; t<tc; t++){
-            result[t] = simulate(t, places[t]);
+        for(int t = 0; t < SIZE; t++){
+            for(int i = 0; i < SIZE; i++){
+                for(int j = 0; j < SIZE; j++){
+                    if(places[t][i].charAt(j) == 'P'){
+                        if(!isPossible(places[t],i,j)){
+                            result[t] = 0;
+                            break;
+                        }
+                    }
+                }
+            }
         }
         
         return result;
         
     }
     
-    private static int simulate(int idx, String [] place){
+    private static boolean isPossible(String[] place, int x, int y){
         
-        visited = new boolean[SIZE][SIZE];
-        
-        for(int i = 0; i<SIZE; i++){
-            for(int j = 0; j<SIZE; j++){
-                if(place[i].charAt(j) == 'P' && !visited[i][j]){
-                    visited[i][j] = true;
-                    if(bfs(place,visited,new Pos(i,j)) == 0) return 0;
-                }
-            }
+        //상하좌우 맨해튼거리 1 ~ 2 확인
+        for(int i = 0; i < 4; i++){
+            int mvX = x+dx[i];
+            int mvY = y+dy[i];
+            //해당 좌표가 대기실 밖이거나 파티션일 경우
+            if(!isIn(mvX,mvY) || place[mvX].charAt(mvY) == 'X') continue;
+            //맨해튼거리가 1인 위치에 응시자가 있을 경우
+            if(place[mvX].charAt(mvY) == 'P') return false;
+            //맨해튼거리가 2인 위치에 응시가자 앉아있을 경우
+            mvX+=dx[i];
+            mvY+=dy[i];
+            if(isIn(mvX,mvY) && place[mvX].charAt(mvY) == 'P') return false;
         }
         
-        return 1;
-    }
-    
-    private static int bfs(String [] place, boolean [][] visited, Pos pos){
-        
-        Deque<Pos> deque = new ArrayDeque<Pos>();
-        deque.add(pos);
-        
-        while(!deque.isEmpty()){
-            
-            Pos mv = deque.poll();
-            
-            for(int i = 0; i<4; i++){
-                
-                int mvX = mv.x + dx[i];
-                int mvY = mv.y + dy[i];
-                
-                if(mvX < 0 || mvY < 0 || mvX >= SIZE || mvY >= SIZE || visited[mvX][mvY] 
-                        || place[mvX].charAt(mvY) == 'X') continue;
-                if(place[mvX].charAt(mvY) == 'P') return 0;
-                
-                if(Math.abs(pos.x - mvX) + Math.abs(pos.y - mvY) >= 2) continue;
-                
-                deque.add(new Pos(mvX,mvY));
-                visited[mvX][mvY] = true;
-                   
-            }
-            
+        //대각선 확인
+        for(int i = 0; i < 4; i++){
+            int mvX = x+daX[i];
+            int mvY = y+daY[i];
+            if(!isIn(mvX,mvY)) continue;
+            if(place[mvX].charAt(mvY) == 'P' && (place[x].charAt(mvY) != 'X' || place[mvX].charAt(y) != 'X')) return false;
         }
         
-        return 1;
+        return true;
         
+    } 
+    
+    private static boolean isIn(int x, int y){
+        
+        if(x < 0 || y < 0 || x >= SIZE || y >=SIZE) return false;
+        return true;
         
     }
-    
     
 }
