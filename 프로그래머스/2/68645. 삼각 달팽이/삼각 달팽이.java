4fc2f97ec5b18067f4c6d6028aa7a7cbc7d14034
Chaeeun Kang <codms7020@naver.com>
2025-11-21 17:10:00 +0900

    [level 2] Title: 삼각 달팽이, Time: 16.20 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,32 +1,31 @@
+import java.util.*;
+
 class Solution {
+    
     public int[] solution(int n) {
         
-        int [][] board = new int[n][n];
-        int [] result = new int[(n*(n+1))/2];
+        int[][] board = new int[n][n];
+        int max = (n*(n+1))/2;
         
-        int num = 1;
-        int x = -1, y = 0;
+        int x = -1 ,y = 0, cnt = 1;
         
-        for(int i = 0; i<n; i++){ //이동 횟수 ⬇️➡️↖️⬇️
-            for(int j = i; j<n; j++){ //이동 횟수 별 채워야 하는 칸 개수
-                //3으로 나누는 이유 ? 이동하는 방향이 총 3가지
-                if(i%3 == 0) x++; 
-                else if(i%3 == 1) y++;
-                else if(i%3 == 2){
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                if(i % 3 == 0) x++;
+                else if(i % 3 == 1) y++;
+                else if(i % 3 == 2){
                     x--;
                     y--;
                 }
-                
-                board[x][y] = num;
-                num++;
-                
+                board[x][y] = cnt;
+                cnt++;
             }
-            
-        }
+        }        
         
+        int[] result = new int[max];
         int idx = 0;
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<n; j++){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j <= i; j++){
                 if(board[i][j] == 0) continue;
                 result[idx] = board[i][j];
                 idx++;
@@ -34,6 +33,6 @@ class Solution {
         }
         
         return result;
-                      
+        
     }
 }
