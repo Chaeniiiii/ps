2c82b8b64094299d7b346089807d99b2323a873f
Chaeeun Kang <codms7020@naver.com>
2025-11-28 17:19:37 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 184.70 ms, Memory: 198 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -2,46 +2,41 @@ import java.util.*;
 
 class Solution {
     
-    private static class Num {
+    private static class Num{
         
         int idx;
         int n;
         
-        private Num(int n,int idx){
-            this.n=n;
-            this.idx=idx;
-        }
-        
-        private int getNum(){
-            return n;
+        private Num(int idx, int n){
+            this.idx = idx;
+            this.n = n;
         }
     }
     
     public int[] solution(int[] numbers) {
         
-        PriorityQueue<Num> pq = new PriorityQueue<>(Comparator.comparingInt(Num::getNum));
-        int [] result = new int[numbers.length];
+        PriorityQueue<Num> pq = new PriorityQueue<>((a,b) -> a.n - b.n);
+        pq.add(new Num(0,numbers[0]));
         
-        for(int i = 0; i<numbers.length; i++){
+        int[] result = new int[numbers.length];
+        Arrays.fill(result,-1);
+        
+        for(int i = 1; i < numbers.length; i++){
             
             int now = numbers[i];
             
-            if(pq.isEmpty() || pq.peek().n >= now) {
-                pq.add(new Num(now,i));
-                continue;
-            }
-            
-            while(true){
-                if(pq.isEmpty() || pq.peek().n >= now) break;
-                Num prev = pq.poll();
+            while(!pq.isEmpty()){
+                Num prev = pq.peek();
+                if(prev.n >= now) break;
+                pq.poll();
                 result[prev.idx] = now;
             }
-            pq.add(new Num(now,i));
+            pq.add(new Num(i,now));
             
         }
         
-        for(Num num : pq) result[num.idx] = -1;
-        
         return result;
+        
+        
     }
 }
