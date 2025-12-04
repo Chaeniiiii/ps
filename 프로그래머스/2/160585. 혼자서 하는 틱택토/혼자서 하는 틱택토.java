a8f08ab86d2f2544ef64e5912c3ef8ad84b03325
Chaeeun Kang <2020112112@dgu.ac.kr>
2025-12-04 16:27:44 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 0.10 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
@@ -1,49 +1,89 @@
 import java.util.*;
 
 class Solution {
+    
+    private static int n,m;
+    private static boolean[][] visited;
+    
+    private static String[] board;
+    private static int[] dx = new int[]{-1,1,0,0,-1,-1,1,1};
+    private static int[] dy = new int[]{0,0,-1,1,-1,1,-1,1};
+    
     public int solution(String[] board) {
         
-        int o = 0;
-        int x = 0;
+        n = board.length;
+        m = board[0].length();
+        this.board = board;
         
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[0].length(); j++){
-                if(board[i].charAt(j) == 'O'){
-                    o++;
-                }
-                else if(board[i].charAt(j) == 'X'){
-                    x++;
+                
+        int cntO = 0, cntX = 0;
+        boolean bingoO = false, bingoX = false;
+        
+        for(int i = 0; i < board[0].length(); i++){
+            for(int j = 0; j < board.length; j++){
+                char c = board[i].charAt(j);
+                visited = new boolean[n][m];
+                switch(c){
+                    case 'O':
+                        cntO++;
+                        if(!visited[i][j]){
+                            visited[i][j] = true;
+                            bingoO |= checkBG(i,j,-1,1);
+                        }
+                        break;
+                    case 'X':
+                        cntX++;
+                        if(!visited[i][j]){
+                            visited[i][j] = true;
+                            bingoX |= checkBG(i,j,-1,1);
+                        }
+                        break;
+                    default:
+                        break;
                 }
             }
         }
         
-        if(x > o) return 0;
-        if(o > x + 1) return 0;
-        
-        if(bingo(board,'O') && x == o) return 0;
-        if(bingo(board,'X') && x + 1 == o) return 0;
+        //1. O가 만들어졌는데 X와 O의 개수가 같을 때
+        //2. X가 만들어졌는데 O의 개수가 X보다 한 개 많을 때
+        //3. X의 개수가 O보다 많을 때
+        //4. O의 개수가 X보다 2개 많을 때
+        if(bingoO && cntO == cntX || bingoO && bingoX) return 0;
+        if(bingoX && cntO == cntX + 1) return 0;
+        if(cntX > cntO) return 0;
+        if(cntO - cntX >= 2) return 0;
         
         return 1;
         
     }
     
-    private static boolean bingo(String[] board, char c){
+    private static boolean checkBG(int x, int y, int dir, int cnt){
         
-        //가로 확인
-        for(int i = 0; i < 3; i++){
-            if(board[i].charAt(0) == c && board[i].charAt(1) == c && board[i].charAt(2) == c) return true;
+        char now = board[x].charAt(y);
+        
+        if(cnt == 3){
+            return true;
         }
         
-        //세로 확인
-        for(int i = 0; i < 3; i++){
-            if(board[0].charAt(i) == c && board[1].charAt(i) == c && board[2].charAt(i) == c) return true;
+        boolean result = false;
+        for(int k = 0; k < 8; k++){
+            
+            int mvX = x + dx[k];
+            int mvY = y + dy[k];
+            
+            if(mvX < 0 || mvY < 0 || mvX >= n || mvY >= m || visited[mvX][mvY] || board[mvX].charAt(mvY) != now) continue;
+            if(dir == -1){
+                visited[mvX][mvY] = true;
+                result |= checkBG(mvX,mvY,k,cnt+1);
+            }
+            else if(dir == k){
+                visited[mvX][mvY] = true;
+                result |= checkBG(mvX,mvY,k,cnt+1);
+            }
+            
         }
-        
-        //대각선 확인
-        if(board[0].charAt(0) == c && board[1].charAt(1) == c && board[2].charAt(2) == c) return true;
-        if(board[0].charAt(2) == c && board[1].charAt(1) == c && board[2].charAt(0) == c) return true;
-        
-        return false;
+
+        return result;
         
     }
 }
