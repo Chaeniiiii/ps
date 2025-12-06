c15137cb6f6dbd77af3bc323b907391d70049dfc
Chaeeun Kang <2020112112@dgu.ac.kr>
2025-12-06 13:45:04 +0900

    [level 2] Title: 더 맵게, Time: 1902.11 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -3,31 +3,28 @@ import java.util.*;
 class Solution {
     public int solution(int[] scoville, int K) {
         
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        
-        for(int scv : scoville) pq.add(scv);
-        
-        int score = 0;
-        int cnt = 0;
-        
-        while(!pq.isEmpty()){
-            
-            if(pq.peek() >= K) return cnt;
-            
-            for(int i = 0; i<2; i++){
-                if(pq.isEmpty()) return score >= K ? cnt : -1;
-                if(i == 1) {
-                    score +=(pq.poll()*2);
-                    pq.add(score);
-                    cnt ++;
-                    score = 0;
-                }
-                else score+=pq.poll();
-            }
-            
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+        for(long s : scoville){
+            pq.add(s);
         }
         
-        return pq.peek() < K ? -1 : cnt;
+        if(pq.peek() >= K) return 0;
+        
+        int cnt = 0;
+        while(pq.size() > 1){
+            
+            long now = pq.poll();
+            if(now >= K) return cnt;
+            long nxt = pq.poll();
+            
+            pq.add(now + nxt*2);
+            cnt++;
+            
+        }
+    
+        if(pq.size() == 0) return -1;
+        if(pq.size() == 1 && pq.peek() < K) return -1;
+        return cnt;
         
     }
 }
