79671f583ec5636e6b8f64fee963043a3601b1ed
Chaeeun Kang <2020112112@dgu.ac.kr>
2025-12-08 04:13:28 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 64.78 ms, Memory: 69.8 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -3,72 +3,73 @@ import java.util.*;
 class Solution {
     
     private static int n,m;
+    private static int[] amount;
+    private static boolean[][] visited;
     
     private static int[] dx = new int[]{-1,1,0,0};
     private static int[] dy = new int[]{0,0,-1,1};
     
-    private static int[] oil;
-    private static int[][] land;
-    private static boolean[][] visited;
-    
     public int solution(int[][] land) {
         
-        this.land = land;
         n = land.length;
         m = land[0].length;
         
-        oil = new int[m];
+        amount = new int[m];
         visited = new boolean[n][m];
+        
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(visited[i][j] || land[i][j] == 0) continue;
+                if(land[i][j] == 0 || visited[i][j]) continue;
                 visited[i][j] = true;
-                bfs(i,j);
+                bfs(i,j,land);
             }
         }
         
-        Arrays.sort(oil);
-        return oil[m-1];
+        Arrays.sort(amount);
+        return amount[m-1];
         
     }
     
-    private static void bfs(int x, int y){
+    private static void bfs(int x, int y, int[][] land){
         
         Deque<int[]> deque = new ArrayDeque<>();
         deque.add(new int[]{x,y});
         
+        int min = y;
+        int max = y;
         int cnt = 0;
-        boolean[] oilVisited = new boolean[m];
-        oilVisited[y] = true;
         
         while(!deque.isEmpty()){
             
             int[] now = deque.poll();
             cnt++;
             
-            for(int k = 0; k < 4; k++){
+            for(int i = 0; i < 4; i++){
+                int mvX = now[0] + dx[i];
+                int mvY = now[1] + dy[i];
                 
-                int mvX = now[0] + dx[k];
-                int mvY = now[1] + dy[k];
-                
-                if(isIn(mvX,mvY)) continue;
-                deque.add(new int[]{mvX,mvY});
+                if(!isIn(mvX,mvY) || land[mvX][mvY] == 0) continue;
                 visited[mvX][mvY] = true;
-                oilVisited[mvY] = true;
+                deque.add(new int[]{mvX,mvY});
+                
+                min = Math.min(min,mvY);
+                max = Math.max(max,mvY);
                 
             }
             
         }
         
-        for(int i = 0; i < m; i++){
-            if(oilVisited[i]) oil[i] += cnt;
+        for(int i = min; i <= max; i++){
+            amount[i] += cnt;
         }
         
     }
     
     private static boolean isIn(int x, int y){
-        if(x < 0 || y < 0 || x >= n || y >= m || land[x][y] == 0 || visited[x][y]) return true;
-        return false;
+        
+        if(x < 0 || y < 0 || x >= n || y >= m || visited[x][y]) return false;
+        return true;
+        
     }
     
 }
