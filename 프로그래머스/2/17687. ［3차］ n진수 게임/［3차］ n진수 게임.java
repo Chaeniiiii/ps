69884794b449d91104fb6922b1d053b504940685
Chaeeun Kang <2020112112@dgu.ac.kr>
2025-12-08 03:48:49 +0900

    [level 2] Title: [3차] n진수 게임, Time: 9.89 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,41 +1,39 @@
+import java.util.*;
+
 class Solution {
     public String solution(int n, int t, int m, int p) {
         
         StringBuilder sb = new StringBuilder();
-        sb.append(0);
-        sb.append(1);
+        sb.append(0).append(1);
         
         int num = 2;
-        while(sb.length() <= t*m){
-            
-            String str = getNum(n,num);
-            for(char c : str.toCharArray()){
-                sb.append(c);
-            }
+        while(sb.length() < t * m){
+            sb.append(getNum(num,n));
             num++;
-            
         }
         
+        int st = p - 1;
         StringBuilder result = new StringBuilder();
-        int st = p-1;
+        
         while(result.length() < t){
             result.append(sb.charAt(st));
             st+=m;
         }
+        
         return result.toString();
         
     }
     
-    private static String getNum(int n, int num){
+    private static String getNum(int num, int n){
         
         StringBuilder sb = new StringBuilder();
+        int div;
         while(num > 0){
-            int div = num % n;
+            div = num % n;
             num /= n;
             
             if(div >= 10) sb.append((char)('A' + (div - 10)));
             else sb.append(div);
-            
         }
         
         return sb.reverse().toString();
