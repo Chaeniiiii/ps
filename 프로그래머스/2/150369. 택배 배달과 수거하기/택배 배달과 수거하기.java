dbd40552c2604a91ed8dbd91469602a6715026a4
Chaeeun Kang <2020112112@dgu.ac.kr>
2025-12-09 13:20:49 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 9.66 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -2,25 +2,27 @@ import java.util.*;
 
 class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
-
-        long result = 0;
-        int d = 0, p = 0;
+        
+        int d = 0;
+        int p = 0;
+        
+        long cnt = 0;
         
         for(int i = n - 1; i >= 0; i--){
             
             d -= deliveries[i];
             p -= pickups[i];
             
+            
             while(d < 0 || p < 0){
-                d+=cap;
-                p+=cap;
-                
-                result += (i + 1) * 2; 
+                d += cap;
+                p += cap;
+                cnt+=((i+1)*2);
             }
             
         }
         
-        return result;
+        return cnt;
         
     }
 }
