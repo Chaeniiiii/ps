6ec5fe3dba864ba92eba51e359637ada2016237e
Chaeeun Kang <2020112112@dgu.ac.kr>
2025-12-10 10:51:04 +0900

    [level 2] Title: 괄호 회전하기, Time: 27.07 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -3,26 +3,22 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         
-        int cnt = 0;
-        for(int i = 0; i<s.length(); i++){
+        int n = s.length();
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++){
+            sb.append(s.charAt(i));
+        }
+        
+        int lt = 0, rt = n - 1, cnt = 0;
+        while(lt < n){
             
-            String newStr = s.substring(i) + s.substring(0,i);
+            if(isCorrect(sb.toString())) cnt++;
+            lt++;
+            rt = (rt + 1) % n;
             
-            Deque<Character> deque = new ArrayDeque<>();
-            boolean check = false;
-            
-            for(char c : newStr.toCharArray()){
-                
-                if(c == '(' || c == '{' || c == '[') deque.add(c);
-                else {
-                    if(deque.isEmpty() || !match(deque.pollLast(),c)){
-                        check = true;
-                        break;
-                    }
-                }
-            }
-            
-            if(!check && deque.isEmpty()) cnt ++;
+            sb.deleteCharAt(0);
+            sb.append(s.charAt(rt));
             
         }
         
@@ -30,10 +26,28 @@ class Solution {
         
     }
     
-    private static boolean match(char a, char b){
+    private static boolean isCorrect(String str){
         
-        if(a == '(' && b == ')' || a == '{' && b == '}' || a == '[' && b == ']') return true;
-        return false; 
+        Deque<Character> deque = new ArrayDeque<>();
+        
+        for(int i = 0; i < str.length(); i++){
+            char c = str.charAt(i);
+            if(c == '{' || c == '[' || c == '('){
+                deque.add(c);
+                continue;
+            }
+            if(deque.isEmpty()) return false;
+            
+            char prev = deque.peekLast();
+            if(c == '}' && prev != '{') return false;
+            if(c == ']' && prev != '[') return false;
+            if(c == ')' && prev != '(') return false;
+            
+            deque.pollLast();
+            
+        }
+        
+        return deque.size() == 0 ? true : false;
+        
     }
-    
 }
