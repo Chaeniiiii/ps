8ffee7ef2cfe62d8b26d46a5c73c871957788a0f
Chaeeun Kang <2020112112@dgu.ac.kr>
2025-12-11 15:17:36 +0900

    [level 2] Title: H-Index, Time: 1.04 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,28 +1,42 @@
 import java.util.*;
 
 class Solution {
+    
+    private static int[] c;
+    
     public int solution(int[] citations) {
         
-        int len = citations.length;
+        c = citations;
         Arrays.sort(citations);
         
-        int hIdx = len;
+        int lt = 0, rt = c[c.length - 1];
+        int result = 0;
         
-        while(hIdx >= 0){
+        while(lt <= rt){
             
-            for(int i = 0; i<len; i++){
-                if(citations[i] >= hIdx){
-                    if(i <= hIdx && (len-i) >= hIdx) return hIdx;
-                    break;
-                }
+            int mid = (lt + rt)/2;
+            
+            if(isPossible(mid)){
+                lt = mid + 1;
+                result = Math.max(result,mid);
             }
-            
-            hIdx --;
-             
-            
+            else{
+                rt = mid - 1; 
+            }
         }
         
-        return hIdx;
+        return result;
+        
+    }
+    
+    private static boolean isPossible(int hIndex){
+        
+        int cnt = 0;
+        for(int i = 0; i < c.length; i++){
+            if(c[i] >= hIndex) cnt++;
+        }
+        
+        return cnt >= hIndex ? true : false;
         
     }
 }
