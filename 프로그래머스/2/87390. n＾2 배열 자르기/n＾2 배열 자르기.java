566d2dabf18bbf847e3230cf390b8312c54edb9b
Chaeeun Kang <2020112112@dgu.ac.kr>
2025-12-11 11:33:31 +0900

    [level 2] Title: n^2 배열 자르기, Time: 8.00 ms, Memory: 93.5 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -6,16 +6,13 @@ class Solution {
         int lt = (int)left;
         int rt = (int)right;
         
-        int[] result = new int[rt -  lt + 1];
+        int[] result = new int[rt-lt+1];
         for(long i = left; i <= right; i++){
-            long q = i / n; 
-            long d = i % n; 
             
-            long st = q + 1; 
-            long cnt = 0; 
-            if(d >= st)  cnt = Math.abs(d - st) + 1;
-            int idx = (int)(i - lt);
-            result[idx] = (int)(st + cnt);
+            int p = (int)(i / n);
+            int d = (int)(i % n);
+            
+            result[(int)(i-left)] = Math.max(p,d) + 1;
             
         }
         
