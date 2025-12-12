db2819cce7b22388855c5e0101b7ebebc717cc3a
Chaeeun Kang <2020112112@dgu.ac.kr>
2025-12-12 11:32:36 +0900

    [level 2] Title: 행렬의 곱셈, Time: 10.23 ms, Memory: 95.3 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -6,14 +6,12 @@ class Solution {
         int an = arr1.length;
         int am = arr1[0].length;
         
-        int bn = arr2.length;
         int bm = arr2[0].length;
         
         int[][] result = new int[an][bm];
-        
         for(int i = 0; i < an; i++){
             for(int j = 0; j < bm; j++){
-                for(int k = 0; k < bn; k++){
+                for(int k = 0; k < am; k++){
                     result[i][j] += (arr1[i][k] * arr2[k][j]);
                 }
             }
