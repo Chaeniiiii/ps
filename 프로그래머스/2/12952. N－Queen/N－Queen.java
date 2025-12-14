dd8141b3bde831136f3cadbaab4d4114cc4b2e84
Chaeeun Kang <2020112112@dgu.ac.kr>
2025-12-14 17:01:52 +0900

    [level 2] Title: N-Queen, Time: 139.74 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -2,64 +2,67 @@ import java.util.*;
 
 class Solution {
     
-    private static int n,result;
     private static boolean[][] visited;
     private static boolean[] col;
     
-    
     public int solution(int n) {
-
-        this.n = n;
-        result = 0;
         
-        visited = new boolean[n][n];
         col = new boolean[n];
-        dfs(-1,-1);
+        visited = new boolean[n][n];
         
-        return result;
+        return dfs(n,0);
         
     }
     
-    private static void dfs(int x, int y){
+    private static int dfs(int n , int dep){
         
-        if(x == n - 1){
-            result++;
-            return;
+        int cnt = 0;
+        
+        if(dep == n){
+            return 1;
         }
         
-        for(int nxt = 0; nxt < n; nxt++){
+        for(int i = 0; i < n; i++){
             
-            //대각선 왼쪽 위
-            if(!LT(x+1,nxt)) continue;
-            //대각선 오른쪽 위 
-            if(!RT(x+1,nxt)) continue;
-            //열
-            if(col[nxt]) continue;
+            if(col[i] || visited[dep][i] || !mvl(dep,i,n) || !mvr(dep,i,n)) continue;
+            col[i] = true;
+            visited[dep][i] = true;
+            cnt += dfs(n,dep+1);
+            col[i] = false;
+            visited[dep][i] = false;
             
-            col[nxt] = true;
-            visited[x+1][nxt] = true;
-            dfs(x+1,nxt);
-            visited[x+1][nxt] = false;
-            col[nxt] = false;
         }
-    }
-    
-    private static boolean LT(int x, int y){
         
-        while(x >= 0 && y >= 0){
-            if(visited[x][y]) return false;
-            x--;
-            y--;
-        }
-        return true;
+        return cnt;
+        
     }
     
-    private static boolean RT(int x, int y){
-        while(x >= 0 && y < n){
-            if(visited[x][y]) return false;
-            x--;
-            y++;
+    private static boolean mvl(int x, int y, int n){
+        
+        for(int i = x; i > 0; i--){
+            int mx = i - 1;
+            y -= 1;
+            
+            if(mx < 0 || y < 0) break;
+            if(visited[mx][y]) return false;
         }
+        
         return true;
+        
     }
+    
+    private static boolean mvr(int x, int y, int n){
+        
+        for(int i = x; i > 0; i--){
+            int mx = i - 1;
+            y += 1;
+            
+            if(mx < 0 || y >= n) break;
+            if(visited[mx][y]) return false;
+        }
+        
+        return true;
+        
+    }
+    
 }
