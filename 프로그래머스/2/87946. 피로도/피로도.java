66c85ea8aa935b0ea664565453575f1fd548fceb
Chaeeun Kang <2020112112@dgu.ac.kr>
2025-12-14 16:27:51 +0900

    [level 2] Title: 피로도, Time: 3.53 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -2,37 +2,35 @@ import java.util.*;
 
 class Solution {
     
-    private static int n,result;
-    
-    private static int[][] dungeons;
+    private static int n;
+    private static int[][] d;
     private static boolean[] visited;
     
     public int solution(int k, int[][] dungeons) {
+            
+        d = dungeons;
+        n = d.length;
         
-        n = dungeons.length;
-        result = 0;
-        
-        this.dungeons = dungeons;
         visited = new boolean[n];
         
-        dfs(-1,0,k);
-        return result;
+        return dfs(k,0);
         
     }
     
-    private static void dfs(int st, int dep, int k){
+    private static int dfs(int k, int dep){
         
-        result = Math.max(result,dep);
+        int cnt = 0;
         
-        for(int i = 0; i < n; i++){
-        
-            if(visited[i] || dungeons[i][0] > k) continue;
+        for(int i = 0; i < d.length; i++){
+            
+            if(d[i][0] > k || visited[i]) continue;
             visited[i] = true;
-            dfs(i,dep+1,k-dungeons[i][1]);
+            cnt = Math.max(cnt,dfs(k-d[i][1],dep+1));
             visited[i] = false;
             
         }
         
+        return Math.max(cnt,dep);
+        
     }
-    
 }
