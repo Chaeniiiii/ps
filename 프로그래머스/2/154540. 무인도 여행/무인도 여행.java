bc477862bee1ae7ddecd6264b9ede49e136e7f7e
Chaeeun Kang <2020112112@dgu.ac.kr>
2025-12-14 16:18:18 +0900

    [level 2] Title: 무인도 여행, Time: 7.87 ms, Memory: 91.6 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -2,68 +2,70 @@ import java.util.*;
 
 class Solution {
     
-    private static ArrayList<Integer> arr;
-    private static boolean [][] visited;
+    private static int n,m;
+    private static String[] maps;
+    private static boolean[][] visited;
     
-    private static int col,row;
-    
-    private static int [] dx = {-1,1,0,0};
-    private static int [] dy = {0,0,-1,1};
+    private static int[] dx = new int[]{-1,1,0,0};
+    private static int[] dy = new int[]{0,0,-1,1};
     
     public int[] solution(String[] maps) {
-
-        row = maps.length;
-        col = maps[0].length();
         
-        arr = new ArrayList<>();
-        visited = new boolean[maps.length][maps[0].length()];
+        n = maps.length;
+        m = maps[0].length();
         
-        for(int i = 0; i<row; i++){
-            for(int j = 0; j<col; j++){
-                if(maps[i].charAt(j) != 'X' && !visited[i][j]){
-                    visited[i][j] = true;
-                    bfs(i,j,maps);
-                }
+        this.maps = maps;
+        visited = new boolean[n][m];
+        
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                char c = maps[i].charAt(j);
+                if(!Character.isDigit(c) || visited[i][j]) continue;
+                visited[i][j] = true;
+                arr.add(bfs(i,j));
             }
         }
         
         if(arr.size() == 0) return new int[]{-1};
         
-        arr.sort((a,b) -> a-b);
-        int [] result = new int[arr.size()];
-        for(int i = 0; i<arr.size(); i++) result[i] = arr.get(i);
+        int[] result = new int[arr.size()];
+        for(int i = 0; i < result.length; i++){
+            result[i] = arr.get(i);
+        }
         
+        Arrays.sort(result);
         return result;
+        
     }
     
-    private static void bfs(int x, int y, String [] maps){
+    private static int bfs(int x, int y){
         
         Deque<int[]> deque = new ArrayDeque<>();
         deque.add(new int[]{x,y});
         
-        int cnt = maps[x].charAt(y) - '0';
-        
+        int cnt = 0;
         while(!deque.isEmpty()){
-        
-            int [] mv = deque.poll();
             
-            for(int i = 0; i<4; i++){
+            int[] now = deque.poll();
+            int nx = now[0];
+            int ny = now[1];
+            cnt+= (int)maps[nx].charAt(ny) - '0';
+            
+            for(int i = 0; i < 4; i++){
+                int mvX = nx + dx[i];
+                int mvY = ny + dy[i];
                 
-                int mvX = mv[0] + dx[i];
-                int mvY = mv[1] + dy[i];
-                
-                if(mvX < 0 || mvY < 0 || mvX >= row || mvY >= col || 
-                    maps[mvX].charAt(mvY) == 'X' || visited[mvX][mvY]) continue;
-                
+                if(mvX < 0 || mvY < 0 || mvX >= n || mvY >= m || visited[mvX][mvY]) continue;
+                if(!Character.isDigit(maps[mvX].charAt(mvY))) continue;
                 visited[mvX][mvY] = true;
                 deque.add(new int[]{mvX,mvY});
-                cnt += (int)(maps[mvX].charAt(mvY) - '0');
-                
             }
             
         }
         
-        arr.add(cnt);
+        return cnt;
         
     }
+    
 }
