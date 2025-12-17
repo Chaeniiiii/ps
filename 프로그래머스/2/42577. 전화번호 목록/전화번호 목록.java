9e501697dd05918f09df1b8d0d78f750c5a0ffc4
Chaeeun Kang <2020112112@dgu.ac.kr>
2025-12-17 22:36:27 +0900

    [level 2] Title: 전화번호 목록, Time: 302.21 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,26 +1,12 @@
 import java.util.*;
-
 class Solution {
     public boolean solution(String[] phone_book) {
-        
-        Map<String,Integer> map = new HashMap<>();
-        map.put(phone_book[0],1);
-        
         Arrays.sort(phone_book);
-        
-        for(int i = 1; i<phone_book.length; i++){
-            
-            String prev = phone_book[i-1];
+        for(int i = 1; i < phone_book.length; i++){
             String now = phone_book[i];
-            
-            if(now.length() < prev.length()) continue;
-            if(now.substring(0,prev.length()).equals(prev)) return false;
-            
-            map.put(now,map.getOrDefault(now,0)+1);
-            
+            String prev = phone_book[i-1];
+            if(now.startsWith(prev)) return false;
         }
-        
         return true;
-        
     }
 }
