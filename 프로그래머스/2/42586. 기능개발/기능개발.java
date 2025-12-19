b9ba18398ac3ea84884819236d85edbb3499fb1b
Chaeeun Kang <2020112112@dgu.ac.kr>
2025-12-19 20:22:17 +0900

    [level 2] Title: 기능개발, Time: 0.06 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,41 +3,41 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         
-        int n = progresses.length, cnt = 1, prev = calc(progresses[0],speeds[0]);
-        ArrayList<Integer> arr = new ArrayList<>();
-
-        for(int i = 1; i < n; i++){
-            
-            int now = calc(progresses[i],speeds[i]);
-            if(now <= prev){
-                cnt++;
-            }
-            else{
-                arr.add(cnt);
-                prev = now;
-                cnt = 1;
-            }
-            
-            if(i == n - 1) arr.add(cnt);
-            
+        int n = progresses.length;
+        
+        int[] p = new int[n];
+        for(int i = 0; i < n; i++){
+            p[i] = 100 - progresses[i];
         }
         
+        System.out.println(Arrays.toString(p));
+        
+        int prev = 0, cnt = 0, div;
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for(int i = 0; i < n; i++){
+            
+            div = (p[i] / speeds[i]) + ((p[i] % speeds[i]) == 0 ? 0 : 1);
+            System.out.printf("%d %d\n",div,prev);
+            
+            if(div <= prev){
+                cnt++;
+                arr.add(cnt);
+                cnt = 1;
+            }
+            else{
+                cnt++;
+            }
+            prev = div;
+        }
+        if(cnt > 1) arr.add(cnt);
+        
         int[] result = new int[arr.size()];
-        for(int i = 0; i < result.length; i++){
+        for(int i = 0; i < arr.size(); i++){
             result[i] = arr.get(i);
         }
         
         return result;
-        
-    }
-    
-    private static int calc(int prg, int spd){
-        
-        prg = 100 - prg;
-        int p = prg / spd;
-        int d = prg % spd;
-        
-        return p + (d == 0 ? 0 : 1);
         
     }
 }
