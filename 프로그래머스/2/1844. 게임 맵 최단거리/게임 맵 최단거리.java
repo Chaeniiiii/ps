7acc1cbedb8aab68e9222c73a35d1a7686907d37
Chaeeun Kang <2020112112@dgu.ac.kr>
2025-12-19 21:42:39 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.19 ms, Memory: 58.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,9 +2,10 @@ import java.util.*;
 
 class Solution {
     
-    private static class Pos {
+    private static class Pos{
         int x;
         int y;
+        
         private Pos(int x, int y){
             this.x = x;
             this.y = y;
@@ -13,46 +14,35 @@ class Solution {
     
     public int solution(int[][] maps) {
         
-        int n = maps.length;
-        int m = maps[0].length;
+        int n = maps.length, m = maps[0].length;
+        boolean[][] visited = new boolean[n][m];
+        
+        Deque<Pos> deque = new ArrayDeque<>();
+        deque.add(new Pos(0,0));
+        visited[0][0] = true;
         
         int[] dx = new int[]{-1,1,0,0};
         int[] dy = new int[]{0,0,-1,1};
         
-        Deque<Pos> deque = new ArrayDeque<>();
-        deque.add(new Pos(0,0));
-        
-        boolean[][] visited = new boolean[n][m];
-        visited[0][0] = true;
-        
-        int cnt = 1;
+        int cnt = 0;
         while(!deque.isEmpty()){
             
             int size = deque.size();
-            
-            for(int i = 0; i<size; i++){
+            for(int i = 0; i < size; i++){
+                Pos now = deque.poll();
                 
-                Pos mv = deque.poll();
-                if(mv.x == n-1 && mv.y == m-1) return cnt;
+                if(now.x == n - 1 && now. y == m - 1) return cnt + 1;
                 
-                for(int k = 0; k<4; k++){
+                for(int k = 0; k < 4; k++){
+                    int mvX = now.x + dx[k];
+                    int mvY = now.y + dy[k];
                     
-                    int mvX = mv.x + dx[k];
-                    int mvY = mv.y + dy[k];
-                    
-                    if(mvX < 0 || mvY < 0 || mvX >= n || mvY >= m || maps[mvX][mvY] == 0 || visited[mvX][mvY]){
-                        continue;
-                    }
-                    
+                    if(mvX < 0 || mvY < 0 || mvX >= n || mvY >= m || visited[mvX][mvY] || maps[mvX][mvY] == 0) continue;
                     deque.add(new Pos(mvX,mvY));
                     visited[mvX][mvY] = true;
-                    
                 }
-                
             }
-            
             cnt++;
-            
         }
         
         return -1;
