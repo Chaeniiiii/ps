a745179d4f945dd89679fabe89c76d0bd999b6bd
Chaeeun Kang <2020112112@dgu.ac.kr>
2025-12-19 20:22:00 +0900

    [level 2] Title: 기능개발, Time: 0.06 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,37 +3,41 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         
-        Deque<Integer> deque = new ArrayDeque<>();
+        int n = progresses.length, cnt = 1, prev = calc(progresses[0],speeds[0]);
         ArrayList<Integer> arr = new ArrayList<>();
-        
-        for(int i = 0; i<progresses.length; i++){
+
+        for(int i = 1; i < n; i++){
             
-            int day = (100-progresses[i])/speeds[i];
-            int divide = (100-progresses[i])%speeds[i];
-            
-            if(divide != 0) deque.add(day+1);
-            else deque.add(day);
-            
-        }
-        
-        while(!deque.isEmpty()){
-            
-            int now = deque.poll();
-            int cnt = 1;
-            
-            while(!deque.isEmpty() && now >= deque.peek()){
-                deque.poll();
+            int now = calc(progresses[i],speeds[i]);
+            if(now <= prev){
                 cnt++;
             }
+            else{
+                arr.add(cnt);
+                prev = now;
+                cnt = 1;
+            }
             
-            arr.add(cnt);
+            if(i == n - 1) arr.add(cnt);
             
         }
         
-        int [] result = new int[arr.size()];
-        for(int i = 0; i<result.length; i++) result[i] = arr.get(i);
+        int[] result = new int[arr.size()];
+        for(int i = 0; i < result.length; i++){
+            result[i] = arr.get(i);
+        }
         
         return result;
+        
+    }
+    
+    private static int calc(int prg, int spd){
+        
+        prg = 100 - prg;
+        int p = prg / spd;
+        int d = prg % spd;
+        
+        return p + (d == 0 ? 0 : 1);
         
     }
 }
