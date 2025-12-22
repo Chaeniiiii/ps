7a4901b1d42eae568653a3a105aabf7ed698a7be
Chaeeun Kang <2020112112@dgu.ac.kr>
2025-12-22 18:02:04 +0900

    [level 2] Title: 방문 길이, Time: 0.43 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -2,100 +2,57 @@ import java.util.*;
 
 class Solution {
     
-    private static final int SIZE = 10;
-    
-    private static class Move{
-        
-        Pos st;
-        Pos en;
-        
-        private Move(Pos st, Pos en){
-            if(st.x < en.x || (st.x == en.x && st.y < en.y)){
-                this.st = st;
-                this.en = en;
-            }
-            else{
-                this.st = en;
-                this.en = st;
-            }
-        }
-        
-        @Override
-        public int hashCode(){
-            return Objects.hash(st,en);
-        }
-        
-        @Override
-        public boolean equals(Object obj){
-            if(this == obj) return true;
-            if(obj == null || getClass() != obj.getClass()) return false;
-            Move mv = (Move)obj;
-            return st.equals(mv.st) && en.equals(mv.en);       
-        }
-        
-    }
-    
-    private static class Pos{
-        
-        private int x;
-        private int y;
-        
-        private Pos(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-        
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) return true;
-            if (obj == null || getClass() != obj.getClass()) return false;
-            Pos p = (Pos) obj;
-            return x == p.x && y == p.y;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
-        
-    }
-    
+    private static final int MAX = 10;
+    private static int result = 0;
+    private static int[] dx = new int[]{-1,0,1,0};
+    private static int[] dy = new int[]{0,-1,0,1};
+    private static boolean[][][] visited ;
     public int solution(String dirs) {
         
-        Set<Move> set = new HashSet<>();
-        Pos pos = new Pos(5,5);
+        visited = new boolean[MAX+1][MAX+1][4];
         
-        int cnt = 0;
+        int [] mv = new int[]{5,5};
         for(char c : dirs.toCharArray()){
-            Pos nxt = new Pos(pos.x,pos.y);
+            
             switch(c){
-                case 'U':
-                    nxt = new Pos(pos.x-1,pos.y);
+                case 'U' :
+                    mv = move(mv[0],mv[1],0);
                     break;
-                case 'D':
-                    nxt = new Pos(pos.x+1,pos.y);
+                case 'D' :
+                    mv = move(mv[0],mv[1],2);
+                    break;
+                case 'L' :
+                    mv = move(mv[0],mv[1],1);
                     break;
                 case 'R':
-                    nxt = new Pos(pos.x,pos.y+1);
+                    mv = move(mv[0],mv[1],3);
                     break;
-                case 'L':
-                    nxt = new Pos(pos.x,pos.y-1);
-                    break;
+                    
             }
-            
-            if(nxt.x < 0 || nxt.y < 0 || nxt.x > SIZE || nxt.y > SIZE) continue;
-            
-            Move mv = new Move(pos,nxt);
-            if(!set.contains(mv)){ 
-                cnt++;
-                set.add(mv);
-            }
-            
-            pos = nxt;
-            
         }
         
-        return cnt;
+        return result;
         
     }
+    
+    private static int[] move(int x, int y, int dir){
+        
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+        int other = (dir+2) % 4;
+        
+        if(!isIn(nx,ny)) return new int[]{x,y};
+        if(!visited[x][y][other] && !visited[nx][ny][dir]) result++;
+        visited[x][y][other] = visited[nx][ny][dir] = true;
+        return new int[]{nx,ny};
+        
+    }
+    
+    private static boolean isIn(int x, int y){
+        
+        if(x < 0 || y < 0 || x > MAX || y > MAX) return false;
+        return true;
+        
+    }
+    
 }
