29d9844ca27ac48dcd84355cf5b8ffb41f839885
Chaeeun Kang <2020112112@dgu.ac.kr>
2025-12-22 18:57:37 +0900

    [level 2] Title: 롤케이크 자르기, Time: 238.25 ms, Memory: 168 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,27 +1,30 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] topping) {
         
-        Map<Integer,Integer> map1 = new HashMap<>();
-        Map<Integer,Integer> map2 = new HashMap<>();
+        int cnt = 0;
+        Map<Integer,Integer> rightM = new HashMap<>();
+        Map<Integer,Integer> leftM = new HashMap<>();
         
-        map1.put(topping[0],1);
-        for(int i = 1 ; i < topping.length; i++){
-            map2.put(topping[i],map2.getOrDefault(topping[i],0)+1);
+        for(int i : topping){
+            rightM.put(i,rightM.getOrDefault(i,0)+1);            
         }
         
-        int cnt = 0;
-        for(int i = 1 ; i < topping.length; i++){
-            map1.put(topping[i],map1.getOrDefault(topping[i],0)+1);
-            if(map2.get(topping[i]) == 1) map2.remove(topping[i]);
-            else map2.put(topping[i],map2.get(topping[i])-1);
+        for(int tp : topping){
+    
+            leftM.put(tp,leftM.getOrDefault(tp,0)+1);
             
-            if(map1.keySet().size() == map2.keySet().size()) cnt++;
+            if(rightM.get(tp) == 1){
+                rightM.remove(tp);
+            }
+            else rightM.put(tp,rightM.get(tp)-1);
             
+            if(leftM.size() == rightM.size()) cnt++;
+                
         }
         
         return cnt;
-        
         
     }
 }
