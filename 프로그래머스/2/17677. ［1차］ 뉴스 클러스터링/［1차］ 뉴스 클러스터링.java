83b3e7b3d72452a77e8db575ee4f79c385523655
Chaeeun Kang <2020112112@dgu.ac.kr>
2025-12-22 18:45:13 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 11.09 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -2,49 +2,46 @@ import java.util.*;
 
 class Solution {
     
-    private static final int MAX = 65536;
+    private static final int NUM = 65536;
     
     public int solution(String str1, String str2) {
-    
-        Map<String,Integer> map1 = new HashMap<>();
-        Map<String,Integer> map2 = new HashMap<>();
         
-        map1 = setMap(str1,map1);
-        map2 = setMap(str2,map2);
-
-        int itsec = 0;
+        Map<String,Integer> map1 = getStr(str1.toUpperCase());
+        Map<String,Integer> map2 = getStr(str2.toUpperCase());
+        
+        //합집합 찾기
         int union = 0;
-       
         for(String key : map1.keySet()){
-            
-            if(map2.get(key) != null){
-                itsec+=Math.min(map1.get(key),map2.get(key));
-                union+=Math.max(map1.get(key),map2.get(key));
-            }
-            else{
-                union+=map1.get(key);
-            }
-            
+            if(map2.containsKey(key)) union += Math.max(map1.get(key),map2.get(key));
+            else union += map1.get(key);
         }
-        
         for(String key : map2.keySet()){
-            if(map1.get(key) == null) union+=map2.get(key);
+            if(map1.containsKey(key)) continue;
+            else union += map2.get(key);
+        }
+
+        //교집합 찾기
+        int sec = 0;
+        for(String key : map1.keySet()){
+            if(map2.containsKey(key)) sec+= Math.min(map1.get(key),map2.get(key));
         }
         
-        if(itsec == 0 && union == 0 ) return MAX;
-        return (int) (((double) itsec / union) * MAX);
+        if(union == 0) return NUM;
+        return (int)(((double)sec/union) * NUM);
         
     }
     
-    private static Map<String,Integer> setMap(String str,Map<String,Integer> map){
-        for(int i = 0; i<str.length()-1; i++){
-            String s = str.substring(i,i+2).toUpperCase();
-            if(!s.matches("[A-Z]+")) continue;
-            map.put(s,map.getOrDefault(s,0)+1);
+    private static Map<String,Integer> getStr(String str){
+        
+        Map<String,Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < str.length() - 1; i++){
+            String newStr = str.substring(i,Math.min(str.length(),i+2)).replaceAll("[^A-Z]","");
+            if(newStr.length() != 2) continue;
+            map.put(newStr,map.getOrDefault(newStr,0)+1);
         }
         
         return map;
+        
     }
-    
-   
 }
