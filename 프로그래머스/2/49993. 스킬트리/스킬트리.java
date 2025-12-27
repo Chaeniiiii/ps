b4c91ab7a26b76b79ff2b0540dc5ec20eb5a5f12
Chaeeun Kang <2020112112@dgu.ac.kr>
2025-12-27 22:57:43 +0900

    [level 2] Title: 스킬트리, Time: 0.17 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -2,38 +2,27 @@ import java.util.*;
 
 class Solution {
     public int solution(String skill, String[] skill_trees) {
+       
+        Map<Character,Integer> map = new HashMap<>();
+        for(int i = 0; i < skill.length(); i++){
+            map.put(skill.charAt(i),i);
+        }
         
         int cnt = 0;
-        
-        for(String st : skill_trees){
-            
-            int now = 0;
-            boolean check = true;
-            
-            for(int i = 0; i < st.length(); i++){
-
-                char c = st.charAt(i);
-                int tg = skill.indexOf(c);
-                
-                if(tg == -1) continue;
-                
-                if((now == 0 && tg > 0) || now != tg){
-                    check = false;
+        for(String s : skill_trees){
+            int idx = -1;
+            cnt++;
+            for(char c : s.toCharArray()){
+                if(!map.containsKey(c)) continue;
+                if(idx + 1 != map.get(c)){
+                    cnt--;
                     break;
                 }
-                
-                now = tg+1;
-                
+                idx = map.get(c);
             }
-            
-            if(check){
-                cnt++;
-            }
-            
         }
         
         return cnt;
-
-    }
         
+    }
 }
