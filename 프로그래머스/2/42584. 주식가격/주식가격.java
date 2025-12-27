d6c630482a16fd8f2551478a8157b2503459a58a
Chaeeun Kang <2020112112@dgu.ac.kr>
2025-12-27 22:20:29 +0900

    [level 2] Title: 주식가격, Time: 54.88 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,41 +1,29 @@
 import java.util.*;
 
 class Solution {
-    
-    private static class Num {
-        
-        int idx;
-        int n;
-        
-        private Num(int idx, int n){
-            this.idx = idx;
-            this.n = n;
-        }
-        
-    }
-    
     public int[] solution(int[] prices) {
         
-        Deque<Num> deque = new ArrayDeque<>();
-        int [] result = new int[prices.length];
+        int n = prices.length;
         
-        for(int i = 0; i<prices.length; i++){
-            
-            Num num = new Num(i,prices[i]);
-            int size = deque.size();
-            
-            for(int s = 0 ; s<size; s++){
-                Num next = deque.poll();
-                
-                if(next.n <= num.n) deque.add(next);
-                result[next.idx]++;
-                
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> b[0] - a[0]);
+        pq.add(new int[]{prices[0],0});
+        
+        int[] result = new int[n];
+        
+        for(int i = 1; i < n; i++){
+            int now = prices[i];
+            while(!pq.isEmpty() && pq.peek()[0] > now){
+                int[] prev = pq.poll();
+                result[prev[1]] = i - prev[1];
             }
-            
-            deque.add(num);
-            
+            pq.add(new int[]{now,i});
         }
-                                                    
+        
+        while(!pq.isEmpty()){
+            int[] now = pq.poll();
+            result[now[1]] = n - 1 - now[1];
+        }
+        
         return result;
         
     }
