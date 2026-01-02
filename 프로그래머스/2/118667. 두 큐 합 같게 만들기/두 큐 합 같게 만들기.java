ce22ebecc4d23f8424ed3e7858b079f4b821b3b6
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-01-02 19:45:27 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 51.65 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,44 +1,43 @@
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
     
+    private static final int MAX = 300_000;
+    
     public int solution(int[] queue1, int[] queue2) {
         
-        long sum1 = Arrays.stream(queue1).sum();
-        long sum2 = Arrays.stream(queue2).sum();
-        
-        if((sum1+sum2)%2 != 0) return -1;
-        
-        int n = queue1.length;
+        long sum1 = 0, sum2 = 0, n = queue1.length;
         Deque<Integer> deque1 = new ArrayDeque<>();
         Deque<Integer> deque2 = new ArrayDeque<>();
         
         for(int i = 0; i < n; i++){
             deque1.add(queue1[i]);
             deque2.add(queue2[i]);
+            
+            sum1 += queue1[i];
+            sum2 += queue2[i];
         }
         
-        int cnt = 0;
-        while(cnt <= 3*n){
-            
-            int num;
-            if(sum1 > sum2){
-                num = deque1.poll();
-                sum2+=num;
-                sum1-=num;
-                deque2.add(num);
+        
+        if((sum1 + sum2) % 2 != 0) return -1;
+                
+        int c = MAX * 3, cnt = 0;
+        while(c-- > 0){
+            if(sum1 < sum2){
+                int ele = deque2.poll();
+                deque1.add(ele);
+                sum1 += ele;
+                sum2 -= ele;
             }
-            else if(sum1 < sum2){
-                num = deque2.poll();
-                sum1+=num;
-                sum2-=num;
-                deque1.add(num);
+            else if(sum1 > sum2){
+                int ele = deque1.poll();
+                deque2.add(ele);
+                sum1 -= ele;
+                sum2 += ele;
             }
-            else{
-                return cnt;
-            }
+            else return cnt;
             cnt++;
+            
         }
         
         return -1;
