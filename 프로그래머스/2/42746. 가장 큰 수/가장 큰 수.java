7ee6fab31d3f4965688743baa9afdb5759a0af10
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-01-03 23:07:46 +0900

    [level 2] Title: 가장 큰 수, Time: 190.98 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,27 +2,52 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-
-        String[] arr = new String[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-            arr[i] = String.valueOf(numbers[i]);
+        
+        int n = numbers.length;
+        Map<Integer,ArrayList<Integer>> map = new TreeMap<>();
+        
+        for(int i = 0; i < n; i++){
+            String str = String.valueOf(numbers[i]);
+            int num = str.charAt(0) - '0';
+            
+            map.computeIfAbsent(num,v -> new ArrayList<>()).add(numbers[i]);
         }
-
-        Arrays.sort(arr, (a, b) -> {
-            String ab = a + b;
-            String ba = b + a;
-            return ba.compareTo(ab); 
-        });
-
-        if (arr[0].equals("0")) {
-            return "0";
+        
+        for(int key : map.keySet()){
+            ArrayList<Integer> arr = map.get(key);
+            arr.sort((a,b) -> {
+                String aStr = String.valueOf(a);
+                String bStr = String.valueOf(b);
+                
+                int aLen = aStr.length();
+                int bLen = bStr.length();
+                
+                if(aLen > bLen){
+                    if(aStr.charAt(bLen) > bStr.charAt(bLen - 1)) return b - a;
+                }
+                else if(aLen < bLen){
+                    if(bStr.charAt(aLen) > aStr.charAt(aLen - 1)) return a - b;
+                }
+                else{
+                    if(aStr.charAt(aLen - 1) > bStr.charAt(aLen - 1)) return b - a;
+                    else return a - b;
+                }
+                
+                return a - b;
+            });
         }
-
+        
+        ArrayList<Integer> key = new ArrayList<>(map.keySet());
         StringBuilder sb = new StringBuilder();
-        for (String s : arr) {
-            sb.append(s);
+        
+        for(int i = key.size() - 1; i >= 0; i--){
+            ArrayList<Integer> arr = map.get(key.get(i));
+            for(int j = 0 ; j < arr.size(); j++){
+                sb.append(arr.get(j));
+            }
         }
 
-        return sb.toString();
+        return sb.toString().charAt(0) == '0' ? "0" : sb.toString(); 
+        
     }
 }
