d33aa9bc93dd95148ecd63ab5527a9c56688af6f
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-01-03 23:07:40 +0900

    [level 2] Title: 가장 큰 수, Time: 190.98 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,21 +1,28 @@
 import java.util.*;
 
 class Solution {
-
     public String solution(int[] numbers) {
-        
+
         String[] arr = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            arr[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(arr, (a, b) -> {
+            String ab = a + b;
+            String ba = b + a;
+            return ba.compareTo(ab); 
+        });
+
+        if (arr[0].equals("0")) {
+            return "0";
+        }
+
         StringBuilder sb = new StringBuilder();
-        
-        for(int i = 0; i<numbers.length; i++) {
-            arr[i] = (String.valueOf(numbers[i]));       
-        }     
-        
-        Arrays.sort(arr,(a,b) -> (b+a).compareTo(a+b));
-        // if(arr[0].equals("0")) return "0";
-        for(String num : arr) sb.append(num);
-        if(sb.toString().charAt(0) == '0') return "0";
+        for (String s : arr) {
+            sb.append(s);
+        }
+
         return sb.toString();
-        
     }
 }
