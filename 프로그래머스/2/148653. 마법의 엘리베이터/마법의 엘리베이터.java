546436ff89f9c17b069a109730f500ad5211be1c
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-01-08 23:13:31 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.03 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -3,39 +3,29 @@ import java.util.*;
 class Solution {
     public int solution(int storey) {
         
-        int[] num = new int[String.valueOf(storey).length()];
-        int result = 0;
-        
-        String number = String.valueOf(storey);
-        for(int i = 0; i < number.length(); i++){
-            num[i] = number.charAt(i) - '0';
-        }
+        int cnt = 0;
+        while(storey > 0){
             
-        
-        for(int i = num.length - 1; i >= 0; i--){
+            int div = storey % 10;
+            int prev = (storey / 10) % 10;
             
-            int now = num[i];
+            storey /= 10;
             
-            if(now > 5){
-                result += (10 - now);
-                if(i == 0) result++;
-                else num[i - 1]++;
+            if(div > 5){
+                cnt += (10 - div);
+                storey++;
             }
-            else if(now < 5){
-                result += now;
+            else if(div < 5){
+                cnt += div;
             }
             else{
-                result+=5;
-                if(i == 0 || num[i-1] < 5){
-                    continue;
-                }
-                if(num[i-1] >= 5){
-                    num[i-1]++;
-                }
+                cnt+= div;
+                if(prev >= 5) storey++;
             }
+            
         }
         
-        return result;
+        return cnt;
         
     }
 }
