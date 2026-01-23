1635a657fac69b5426fc5ec4cbe753584ddcb4c1
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-01-23 19:53:28 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 15.99 ms, Memory: 89.9 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/README.md
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 정사각형 찾기 - 12905 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12905#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12905) 
 
 ### 성능 요약
 
-메모리: 90.3 MB, 시간: 18.83 ms
+메모리: 89.9 MB, 시간: 15.99 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 12일 00:29:53
+2026년 01월 23일 19:53:25
 
 ### 문제 설명
 
--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
@@ -1,20 +1,32 @@
+import java.util.*;
+
 class Solution {
-    public int solution(int[][] board) {
+    public int solution(int [][]board) {
         
         int n = board.length;
         int m = board[0].length;
-
-        int[][] dp = new int[n + 1][m + 1]; 
-        int max = 0;
+        int result = 0;
         
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (board[i-1][j-1] == 0) continue;
-                dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]),dp[i - 1][j - 1]) + 1;
-                max = Math.max(max, dp[i][j]);
+        int[][] dp = new int[n][m];
+        for(int i = 0; i < n; i++){
+            dp[i][0] = board[i][0];
+            result = Math.max(result,dp[i][0]);
+        }
+        for(int i = 0; i < m; i++){
+            dp[0][i] = board[0][i];
+            result = Math.max(result,dp[0][i]);
+        }
+        
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                if(board[i][j] == 0) continue;
+                dp[i][j] = Math.min(dp[i][j-1],Math.min(dp[i-1][j],dp[i-1][j-1]))+1;
+                result = Math.max(result,dp[i][j]);
             }
         }
-
-        return max * max;
+        
+        return result * result;
+        
     }
+    
 }
