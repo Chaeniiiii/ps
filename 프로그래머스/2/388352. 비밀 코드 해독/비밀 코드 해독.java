8bd93f3553cd3e60b3b1b549df3b83521ce3aed2
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-01-31 13:43:16 +0900

    [level 2] Title: 비밀 코드 해독, Time: 16.21 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
@@ -2,46 +2,60 @@ import java.util.*;
 
 class Solution {
     
-    private static final int SIZE = 5;
-    private static int result;
+    private static int result = 0;
     
-    private static int[][] q;
     private static int[] ans;
-
+    private static int[][] q;
+    private static ArrayList<Integer> arr;
+    
     public int solution(int n, int[][] q, int[] ans) {
+        
+        boolean[] visited = new boolean[n+1];
         
         this.q = q;
         this.ans = ans;
+        arr = new ArrayList<>();
         
-        result = 0;
-        dfs(1,n,0, new boolean[n+1]);
+        for(int i = 1; i <= n; i++){
+            arr.add(i);
+        }
         
+        dfs(0,0,new boolean[n+1]);
         return result;
         
     }
     
-    private static void dfs(int idx, int n, int dep, boolean[] arr){
+    private static void dfs(int dep, int st, boolean[] number){
         
-        if(dep == SIZE ){
-            if(calc(arr)) result++;
+        if(dep == 5){
+            if(isPossible(number)) result++;
             return;
         }
         
-        for(int i = idx; i <= n; i++){
-            arr[i] = true;
-            dfs(i+1,n,dep+1,arr);
-            arr[i] = false;
+        for(int i = st; i < arr.size(); i++){
+            int now = arr.get(i);
+            number[now] = true;
+            dfs(dep+1,i+1,number);
+            number[now] = false;
         }
         
     }
     
-    private static boolean calc(boolean[] arr){
+    private static boolean isPossible(boolean[] number){
+        
+        // ArrayList<Integer> numberArr = new ArrayList<>();
+        // for(int i = 0; i < number.length; i++){
+        //     if(!number[i]) continue;
+        //     numberArr.add(i);
+        //     System.out.printf("%d ",i);
+        // }
+        // System.out.println();
         
         for(int i = 0; i < q.length; i++){
-            int[] query = q[i];
             int cnt = 0;
-            for(int j = 0; j < SIZE; j++){
-                if(arr[query[j]]) cnt++;
+            for(int j = 0; j < q[i].length; j++){
+                int num = q[i][j];
+                if(number[num]) cnt++;
             }
             if(cnt != ans[i]) return false;
         }
