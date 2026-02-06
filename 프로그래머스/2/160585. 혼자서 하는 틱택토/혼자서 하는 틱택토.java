bec4595f34d2b1ad9202d9a8c652832cb4a90bc4
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-02-06 09:52:28 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 0.05 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
@@ -1,89 +1,60 @@
-import java.util.*;
-
 class Solution {
-    
-    private static int n,m;
-    private static boolean[][] visited;
-    
-    private static String[] board;
-    private static int[] dx = new int[]{-1,1,0,0,-1,-1,1,1};
-    private static int[] dy = new int[]{0,0,-1,1,-1,1,-1,1};
-    
+
+    private static final int SIZE = 3;
+
     public int solution(String[] board) {
-        
-        n = board.length;
-        m = board[0].length();
-        this.board = board;
-        
-                
-        int cntO = 0, cntX = 0;
-        boolean bingoO = false, bingoX = false;
-        
-        for(int i = 0; i < board[0].length(); i++){
-            for(int j = 0; j < board.length; j++){
-                char c = board[i].charAt(j);
-                visited = new boolean[n][m];
-                switch(c){
-                    case 'O':
-                        cntO++;
-                        if(!visited[i][j]){
-                            visited[i][j] = true;
-                            bingoO |= checkBG(i,j,-1,1);
-                        }
-                        break;
-                    case 'X':
-                        cntX++;
-                        if(!visited[i][j]){
-                            visited[i][j] = true;
-                            bingoX |= checkBG(i,j,-1,1);
-                        }
-                        break;
-                    default:
-                        break;
-                }
+        char[][] bd = new char[SIZE][SIZE];
+
+        int o = 0, x = 0;
+        boolean makeO = false, makeX = false;
+
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                bd[i][j] = board[i].charAt(j);
+                if (bd[i][j] == 'O') o++;
+                else if (bd[i][j] == 'X') x++;
             }
-        }
-        
-        //1. O가 만들어졌는데 X와 O의 개수가 같을 때
-        //2. X가 만들어졌는데 O의 개수가 X보다 한 개 많을 때
-        //3. X의 개수가 O보다 많을 때
-        //4. O의 개수가 X보다 2개 많을 때
-        if(bingoO && cntO == cntX || bingoO && bingoX) return 0;
-        if(bingoX && cntO == cntX + 1) return 0;
-        if(cntX > cntO) return 0;
-        if(cntO - cntX >= 2) return 0;
-        
-        return 1;
-        
-    }
-    
-    private static boolean checkBG(int x, int y, int dir, int cnt){
-        
-        char now = board[x].charAt(y);
-        
-        if(cnt == 3){
-            return true;
-        }
-        
-        boolean result = false;
-        for(int k = 0; k < 8; k++){
-            
-            int mvX = x + dx[k];
-            int mvY = y + dy[k];
-            
-            if(mvX < 0 || mvY < 0 || mvX >= n || mvY >= m || visited[mvX][mvY] || board[mvX].charAt(mvY) != now) continue;
-            if(dir == -1){
-                visited[mvX][mvY] = true;
-                result |= checkBG(mvX,mvY,k,cnt+1);
-            }
-            else if(dir == k){
-                visited[mvX][mvY] = true;
-                result |= checkBG(mvX,mvY,k,cnt+1);
-            }
-            
         }
 
-        return result;
-        
+        makeO = isWin(bd, 'O');
+        makeX = isWin(bd, 'X');
+
+        return impossible(o, x, makeO, makeX) ? 0 : 1;
+    }
+
+    private static boolean isWin(char[][] bd, char c) {
+
+        // 가로
+        for (int i = 0; i < SIZE; i++) {
+            if (bd[i][0] == c && bd[i][1] == c && bd[i][2] == c)
+                return true;
+        }
+
+        // 세로
+        for (int i = 0; i < SIZE; i++) {
+            if (bd[0][i] == c && bd[1][i] == c && bd[2][i] == c)
+                return true;
+        }
+
+        // 대각선
+        if (bd[0][0] == c && bd[1][1] == c && bd[2][2] == c)
+            return true;
+
+        if (bd[0][2] == c && bd[1][1] == c && bd[2][0] == c)
+            return true;
+
+        return false;
+    }
+
+    private static boolean impossible(int o, int x, boolean makeO, boolean makeX) {
+
+        if (x > o) return true;
+        if (o - x > 1) return true;
+
+        if (makeO && makeX) return true;
+        if (makeO && o != x + 1) return true;
+        if (makeX && o != x) return true;
+
+        return false;
     }
 }
