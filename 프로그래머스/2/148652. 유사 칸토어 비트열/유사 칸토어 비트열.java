2d57eb3bfc97dd6f0af7dec0bc588e334f9081cc
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-02-27 10:25:55 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 104.77 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.java
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.java
@@ -1,27 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n, long l, long r) {
-        
-        int result = 0;
-        
-        for(long i = l - 1; i < r; i++){
-            
-            result += findNum(i);
-            
-        }
-        
-        return result;
-        
+        int answer = 0;
+        return answer;
     }
-    
-    private static int findNum(long i){
-        
-        if(i % 5 == 2) return 0;
-        if(i == 0) return 1;
-        
-        return findNum(i / 5);
-        
-    }
-    
 }
