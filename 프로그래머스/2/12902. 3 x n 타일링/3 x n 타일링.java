e81f624c44cd7f6055569c4d6c1f1ea43c3b563f
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-02-27 13:42:18 +0900

    [level 2] Title: 3 x n 타일링, Time: 0.33 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.java
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.java
@@ -3,21 +3,21 @@ import java.util.*;
 class Solution {
     public int solution(int n) {
         
-        long mod = 1_000_000_007;
-        
-        long[] dp = new long[n+1];
-        dp[0] = 1;
+        int MOD = 1_000_000_007;
+        int[] dp = new int[n+1];
+        dp[1] = 2;
         dp[2] = 3;
         
-        for(int i = 4; i <= n; i+=2){
-            dp[i] = (dp[i-2] * 3) % mod;
-            for(int j = 0; j <= i - 4; j+=2){
-                dp[i] += (dp[j] * 2);
-                dp[i] %= mod;
+        for(int i = 3; i <= n; i++){
+            if(i % 2 == 0){
+                dp[i] = (dp[i - 1] % MOD + dp[i - 2] % MOD) % MOD;
+            }
+            else{
+                dp[i] = ((dp[i - 1] * 2) % MOD + dp[i - 2] % MOD) % MOD;
             }
         }
         
-        return (int)dp[n];
+        return dp[n];
         
     }
 }
