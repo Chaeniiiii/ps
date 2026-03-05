df972966c31f10a4a9ef38d6c980c49f344891a5
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-03-05 18:54:18 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 492.54 ms, Memory: 1.1 GB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -4,106 +4,63 @@ class Solution {
     
     private static final int SIZE = 100;
     
-    private static ArrayList<ArrayList<int[]>> road;
-
-    private static int[][] points;
-    private static int[][] routes;
+    private static int cnt;
+    private static Map<Integer,int[]> map;
+    
+    private static int[][][] visited;
     
     public int solution(int[][] points, int[][] routes) {
         
-        this.points = points;
-        this.routes = routes;
-        road = new ArrayList<>();
-        //각 로봇의 최소 이동 경로 구하기
-        findRoad();
+        cnt = 0;
+        visited = new int[SIZE+1][SIZE+1][SIZE*SIZE+(SIZE*SIZE)];
         
-        //각 로봇이 충돌하는 구간  구하기
-        return findCrash();
+        //각 포인트 좌표 저장
+        map = new HashMap<>();
+        for(int i = 0; i < points.length; i++){
+            map.put(i+1,points[i]);
+        }
+        
+        //p-to-p 최단 경로 저장 
+        for(int i = 0; i < routes.length; i++){
+            int t = 0;
+            findRt(routes[i]);
+        }
+        
+        return cnt;
         
     }
     
-    private static void findRoad(){
+    private static void findRt(int[] r){
         
-        for(int k = 0; k < routes.length; k++){
+        int[] st = map.get(r[0]);
+        int x = st[0];
+        int y = st[1];
+        int[] nxt;
+        int t = 0;
+        if(visited[x][y][t] == 1) cnt++;
+        visited[x][y][t]++;
+        
+        for(int i = 1; i < r.length; i++){
             
-            ArrayList<int[]> arr = new ArrayList<>();
+            nxt = map.get(r[i]);
+            int dx = x < nxt[0] ? 1 : -1; 
+            int dy = y < nxt[1] ? 1 : -1;   
             
-            int[] now = routes[k];
-            int[] st = points[now[0]-1];
-            int stX = st[0];
-            int stY = st[1];
-            arr.add(new int[]{stX,stY});
-            for(int i = 1; i < now.length; i++){
-                
-                int[] en = points[now[i]-1];
-                int enX = en[0];
-                int enY = en[1];
-                
-                while(enX != stX){
-                    if(enX > stX){
-                        stX++;
-                    }
-                    else{
-                        stX--;
-                    }
-                    arr.add(new int[]{stX,stY});
-                }
-                
-                while(enY != stY){
-                    if(enY > stY){
-                        stY++;
-                    }   
-                    else{
-                        stY--;
-                    }
-                    arr.add(new int[]{stX,stY});
-                }
+            while(x != nxt[0] || y != nxt[1]){
+               
+                t++;
+                if(x != nxt[0]) x += dx;
+                else if(y != nxt[1]) y+= dy;
 
-                stX = enX;
-                stY = enY;
+                if(visited[x][y][t] == 1){
+                    cnt++;
+                }
                 
+                visited[x][y][t]++;
+
             }
-            
-            road.add(arr);
             
         }
         
     }
-    
-    private static int findCrash(){
-        
-        int size = 0;
-        for(ArrayList<int[]> arr : road){
-            size = Math.max(size,arr.size());
-        }
-        
-        int result = 0;
-        
-        for(int i = 0; i < size; i++){
-            boolean[] visited = new boolean[road.size()];
-            for(int j = 0; j < road.size(); j++){
-                if(road.get(j).size() <= i || visited[j]) continue;
-                boolean check = false;
-                visited[j] = true;
-                
-                int[] now = road.get(j).get(i);
-                
-                for(int k = j+1; k < road.size(); k++){
-                    if(road.get(k).size() <= i || visited[k]) continue;
-                    int[] nxt = road.get(k).get(i);
-                    if(now[0] == nxt[0] && now[1] == nxt[1]){
-                        check = true;
-                        visited[k] = true;
-                    }
-                }
-                if(check){
-                    result++;
-                }
-            }
-        }
-        
-        return result;
-        
-    }
-    
 }
