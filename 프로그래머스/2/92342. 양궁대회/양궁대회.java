9dfcfeaa2a9fb09987295052ec73a4061b180f91
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-03-06 14:09:56 +0900

    [level 2] Title: 양궁대회, Time: 8.52 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -2,66 +2,66 @@ import java.util.*;
 
 class Solution {
     
-    private static final int size = 10;
+    private static final int SIZE = 10;
     
-    private static int[] info, result;
-    private static int n, diff;
+    private static int diff = 0;
+    
+    private static int[] info;
+    private static int[] lion;
     
     public int[] solution(int n, int[] info) {
         
         this.info = info;
-        this.n = n;
+        lion = new int[SIZE + 1];
         
-        diff = 0;
-        result = new int[size+1];
-        dfs(0,0,new int[size+1]);
+        dfs(0, n, new int[SIZE+1]);
         
-        return diff == 0 ? new int[]{-1} : result;
+        return diff == 0 ? new int[]{-1} : lion;
         
     }
     
-    private static void dfs(int st, int cnt, int[] lion){
+    private static void dfs(int idx, int n, int[] point){
         
-        if(cnt > n) return;
-        if(cnt == n){
-            int l = 0, p = 0;
-            for(int i = 0; i <= size; i++){
-                if(info[i] == 0 && lion[i] == 0) continue;
-                if(info[i] >= lion[i]) p += size - i;
-                else l += size - i;
+        if(n < 0) return;
+        if(n == 0){
+            //점수 계산
+            int a = 0; int l = 0;
+            for(int i = 0; i <= SIZE; i++){
+                int score = SIZE - i;
+                if(info[i] == 0 && point[i] == 0) continue;
+                if(info[i] < point[i]) l+= score;
+                else a += score;
             }
             
-            int newDiff = l - p;
-            if(l > p){
-                if(diff < newDiff){
-                    diff = newDiff;
+            //어피치가 이기거나 점수 차이가 기존보다 적을 경우 제외
+            if(a >= l || diff > Math.abs(a-l)) return;
+            
+            //더 작은 점수를 많이 쏜 경우 판별
+            
+            for(int i = SIZE; i >= 0; i--){
+                if(lion[i] < point[i]){
+                    lion = point.clone();
+                    break;
                 }
-                else if(diff == newDiff){
-                   for(int i = size; i >= 0; i--){
-                        if(lion[i] < result[i]){
-                            return;
-                        }
-                       else if(lion[i] > result[i]) break;
-                   }
-                }
-                else return;
-                result = lion.clone();
+                if(diff == Math.abs(a-l) && lion[i] > point[i]) break;
             }
+            diff = Math.abs(a-l);
             
             return;
-            
         }
         
-        if(st > size) return;
-        
-        //이번 판 진다고 가정
-        dfs(st+1,cnt,lion);
-        //이번 판 이긴다고 가정
-        int brw = st == size ? n - cnt : info[st] + 1;
-        lion[st] = brw;
-        dfs(st+1,cnt+brw,lion);
-        lion[st] = 0;
+        for(int i = idx; i <= SIZE; i++){
+            
+            //라이언이 이기는 경우
+            point[i] = i == SIZE ? n : info[i] + 1;
+            dfs(i+1,n - point[i],point);
+            point[i] = 0;
+            
+            //어피치가 이기는 경우(라이언이 쏘지 않는 경우)
+            dfs(i+1,n,point);
+        }
         
     }
+    
     
 }
