8140faa0851403fe8dffaa4afcb6887fa053f747
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-03-08 19:01:06 +0900

    [level 2] Title: 완전범죄, Time: 0.61 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.java
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -1,41 +1,37 @@
 import java.util.*;
 
 class Solution {
-    
-    private static final int MAX = 120;
-        
     public int solution(int[][] info, int n, int m) {
+
+        if(info.length == 1){
+            return info[0][1] < m ? 0 : info[0][0];
+        } 
         
-        int len = info.length;
-        int[][] dp = new int[len][m];
+        int[][] dp = new int[info.length][m];
         
-        for(int i = 0; i < len; i++){
-            Arrays.fill(dp[i],MAX);
+        for(int i = 0; i < info.length; i++){
+            Arrays.fill(dp[i],120);
         }
-        
         dp[0][0] = info[0][0];
-        if(info[0][1] < m){
-            dp[0][info[0][1]] = 0;
-        }
+        if(info[0][1] < m) dp[0][info[0][1]] = 0; //노이해 
         
-        for(int i = 1; i < len; i++){
+        for(int i = 1; i < info.length; i++){
             for(int j = 0; j < m; j++){
-                dp[i][j] = Math.min(dp[i][j],dp[i-1][j]+info[i][0]);
+                dp[i][j] = Math.min(dp[i][j],dp[i-1][j]+ info[i][0]);
                 
                 if(j + info[i][1] < m){
                     dp[i][j+info[i][1]] = Math.min(dp[i][j+info[i][1]],dp[i-1][j]);
                 }
+                
             }
         }
         
-        int result = MAX;
+        int result = Integer.MAX_VALUE;
         for(int i = 0; i < m; i++){
-            result = Math.min(result,dp[len-1][i]);
+            result = Math.min(result,dp[info.length-1][i]);
         }
         
-        return result >= n ? -1 : result;
+       return result < n ? result : -1; 
         
     }
-    
-    
 }
