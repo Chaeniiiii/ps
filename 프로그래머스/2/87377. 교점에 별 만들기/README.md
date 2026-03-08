5b959f0502934fbfe2514330f4189d3b0edde712
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-03-09 00:12:52 +0900

    [level 2] Title: 교점에 별 만들기, Time: 92.59 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/2/87377. 교점에 별 만들기/README.md
+++ 프로그래머스/2/87377. 교점에 별 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 교점에 별 만들기 - 87377 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87377?gad_source=1&gad_campaignid=22366107751&gbraid=0AAAAAC_c4nDviw1zxzEC1_KiTexSJ-i1g&gclid=Cj0KCQjw9czHBhCyARIsAFZlN8Stv_Cp5et8gwPTj_Tm7YBEkE6BWBZCCZVEmm5V9GWl2SC0-IP4u7AaArnEEALw_wcB) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87377) 
 
 ### 성능 요약
 
-메모리: 111 MB, 시간: 67.07 ms
+메모리: 116 MB, 시간: 92.59 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 18일 23:28:43
+2026년 03월 09일 00:12:50
 
 ### 문제 설명
 
--- 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -1,76 +1,63 @@
 import java.util.*;
 
 class Solution {
-    
-    private static class Pos {
-        long y;
-        long x;
-        
-        private Pos(long y, long x) {
-            this.y = y;
-            this.x = x;
-        }
-    }
-    
+
     public String[] solution(int[][] line) {
-        
-        ArrayList<Pos> arr = new ArrayList<>();
+
+        Set<String> stars = new HashSet<>();
+
         long minX = Long.MAX_VALUE;
         long minY = Long.MAX_VALUE;
         long maxX = Long.MIN_VALUE;
         long maxY = Long.MIN_VALUE;
-        
-        for (int i = 0; i < line.length; i++) {
-            int[] m1 = line[i];
-            long A = m1[0];
-            long B = m1[1];
-            long E = m1[2];
-            
-            for (int j = i + 1; j < line.length; j++) {
-                int[] m2 = line[j];
-                long C = m2[0];
-                long D = m2[1];
-                long F = m2[2];
-                
-                long adbc = A * D - B * C;
-                if (adbc == 0) continue;
-                
-                double x = (double)(B * F - E * D) / adbc;
-                double y = (double)(E * C - A * F) / adbc;
-                
-                if (x % 1 != 0 || y % 1 != 0) continue;
-                
-                long nx = (long)x;
-                long ny = (long)y;
-                
-                arr.add(new Pos(ny, nx));
-                
-                minX = Math.min(minX, nx);
-                minY = Math.min(minY, ny);
-                maxX = Math.max(maxX, nx);
-                maxY = Math.max(maxY, ny);
+
+        for(int i = 0; i < line.length; i++){
+            for(int j = i+1; j < line.length; j++){
+
+                long a = line[i][0];
+                long b = line[i][1];
+                long e = line[i][2];
+
+                long c = line[j][0];
+                long d = line[j][1];
+                long f = line[j][2];
+
+                long parent = a*d - b*c;
+                if(parent == 0) continue;
+
+                long xChild = b*f - e*d;
+                long yChild = e*c - a*f;
+
+                if(xChild % parent != 0 || yChild % parent != 0) continue;
+
+                long x = xChild / parent;
+                long y = yChild / parent;
+
+                stars.add(x + "," + y);
+
+                minX = Math.min(minX, x);
+                minY = Math.min(minY, y);
+                maxX = Math.max(maxX, x);
+                maxY = Math.max(maxY, y);
             }
         }
-        
-        int width = (int)(maxX - minX + 1);
+
         int height = (int)(maxY - minY + 1);
-        
-        String[][] str = new String[height][width];
-        for (int i = 0; i < height; i++) {
-            Arrays.fill(str[i], ".");
+        String[] answer = new String[height];
+
+        for(int i = 0; i < height; i++){
+
+            long y = maxY - i;
+            StringBuilder sb = new StringBuilder();
+
+            for(long x = minX; x <= maxX; x++){
+                if(stars.contains(x + "," + y)) sb.append('*');
+                else sb.append('.');
+            }
+
+            answer[i] = sb.toString();
         }
 
-        for (Pos pos : arr) {
-            int x = (int)(pos.x - minX);
-            int y = (int)(maxY - pos.y); 
-            str[y][x] = "*";
-        }
-
-        String[] result = new String[height];
-        for (int i = 0; i < height; i++) {
-            result[i] = String.join("", str[i]);
-        }
-        
-        return result;
+        return answer;
     }
 }
