34a1a18766cb3082c0310b1a4fca8015b02482ab
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-03-10 15:33:43 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 0.04 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
@@ -1,60 +1,67 @@
+import java.util.*;
+
 class Solution {
-
-    private static final int SIZE = 3;
-
+    
+    private static final int size = 3;
+    
     public int solution(String[] board) {
-        char[][] bd = new char[SIZE][SIZE];
-
-        int o = 0, x = 0;
-        boolean makeO = false, makeX = false;
-
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                bd[i][j] = board[i].charAt(j);
-                if (bd[i][j] == 'O') o++;
-                else if (bd[i][j] == 'X') x++;
+        
+        
+        int o = 0; int x = 0;
+        
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                char c = board[i].charAt(j);
+                if(c == 'O') o++;
+                else if(c == 'X') x++;
             }
         }
-
-        makeO = isWin(bd, 'O');
-        makeX = isWin(bd, 'X');
-
-        return impossible(o, x, makeO, makeX) ? 0 : 1;
+        if(o > x + 1 || o < x) return 0;
+        
+        boolean[] bg = new boolean[2];
+        
+        bingo(board, bg);
+        
+        if(bg[0] && bg[1]) return 0;
+        if(bg[0] && x != o - 1) return 0;
+        if(bg[1] && o != x) return 0;
+        
+        return 1;
+        
     }
-
-    private static boolean isWin(char[][] bd, char c) {
-
-        // 가로
-        for (int i = 0; i < SIZE; i++) {
-            if (bd[i][0] == c && bd[i][1] == c && bd[i][2] == c)
-                return true;
+    
+    private static void bingo(String[] board, boolean[] bg){
+        
+        //가로
+        for(int i = 0; i < size; i++){
+            char st = board[i].charAt(0);
+            if(st == '.') continue;
+            if(board[i].charAt(1) == st && board[i].charAt(2) == st){
+                if(st == 'O') bg[0] |= true;
+                else if(st == 'X') bg[1] |= true;
+            }
         }
-
-        // 세로
-        for (int i = 0; i < SIZE; i++) {
-            if (bd[0][i] == c && bd[1][i] == c && bd[2][i] == c)
-                return true;
+        
+        //세로
+        for(int i = 0; i < size; i++){
+            char st = board[0].charAt(i);
+            if(st == '.') continue;
+            if(board[1].charAt(i) == st && board[2].charAt(i) == st){
+                if(st == 'O') bg[0] |= true;
+                else if(st == 'X') bg[1] |= true;
+            }
         }
-
-        // 대각선
-        if (bd[0][0] == c && bd[1][1] == c && bd[2][2] == c)
-            return true;
-
-        if (bd[0][2] == c && bd[1][1] == c && bd[2][0] == c)
-            return true;
-
-        return false;
-    }
-
-    private static boolean impossible(int o, int x, boolean makeO, boolean makeX) {
-
-        if (x > o) return true;
-        if (o - x > 1) return true;
-
-        if (makeO && makeX) return true;
-        if (makeO && o != x + 1) return true;
-        if (makeX && o != x) return true;
-
-        return false;
+        
+        //대각선 
+        char cl = board[0].charAt(0); char cr = board[0].charAt(2);
+        if(cl == board[1].charAt(1) && cl == board[2].charAt(2)){
+            if(cl == 'O') bg[0] |= true;
+            else if(cl == 'X') bg[1] |= true;
+        }  
+        if(cr == board[1].charAt(1) && cr == board[2].charAt(0)){
+            if(cr == 'O') bg[0] |= true;
+            else if(cr == 'X') bg[1] |= true;
+        }   
+        
     }
 }
