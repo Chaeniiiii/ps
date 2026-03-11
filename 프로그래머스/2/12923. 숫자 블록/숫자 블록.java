d2714387bd7ee7917dbcea636fa643ad792b173a
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-03-11 17:15:34 +0900

    [level 2] Title: 숫자 블록, Time: 306.48 ms, Memory: 55.4 MB -BaekjoonHub

--- 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
+++ 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
@@ -2,33 +2,35 @@ import java.util.*;
 
 class Solution {
     public int[] solution(long begin, long end) {
-
-        int size = (int)(end - begin + 1); 
+        
+        int size = (int)(end - begin + 1);
         int[] result = new int[size];
         
-        Arrays.fill(result,1);
-        result[0] = begin == 1 ? 0 : 1;
-        
-        for(long i = end; i >= begin; i--){
+        for(long i = begin; i <= end; i++){
             
             int idx = (int)(i - begin);
-            if(result[idx] > 1) continue;
+            int max = 1;
             
             for(int j = 2; j <= (int)Math.sqrt(i); j++){
                 int pair;
                 if(i % j == 0){
                     pair = (int)(i / j);
                     if(pair <= 10_000_000){
-                        result[idx] = pair;
+                        max = pair;
                         break;
                     }
-                    else result[idx] = Math.max(result[idx],j);
+                    else{
+                        max = Math.max(max,j);
+                    }
                 }
             }
             
+            result[idx] = max;
+            
         }
         
+        if(begin == 1) result[0] = 0;
         return result;
-        
+                
     }
 }
