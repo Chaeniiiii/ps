fc938a12e774c83c1a861f5c09c032f37b23efa4
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-03-13 09:55:04 +0900

    [level 2] Title: 비밀 코드 해독, Time: 9113.56 ms, Memory: 94.2 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
@@ -2,62 +2,54 @@ import java.util.*;
 
 class Solution {
     
-    private static int result = 0;
-    
-    private static int[] ans;
-    private static int[][] q;
-    private static ArrayList<Integer> arr;
+    private int n, size, cnt;
+    private int[][] q;
+    private int[] ans;
     
     public int solution(int n, int[][] q, int[] ans) {
         
-        boolean[] visited = new boolean[n+1];
-        
+        cnt = 0;
+        size = q[0].length;
+        this.n = n;
         this.q = q;
         this.ans = ans;
-        arr = new ArrayList<>();
         
-        for(int i = 1; i <= n; i++){
+        dfs(1, new ArrayList<>());
+        return cnt;
+        
+    }
+    
+    private void dfs(int idx, ArrayList<Integer> arr){
+        
+        if(arr.size() == size){
+            if(isPossible(arr)){
+                cnt++;
+                return;
+            }
+        }
+        
+        for(int i = idx; i <= n; i++){
             arr.add(i);
-        }
-        
-        dfs(0,0,new boolean[n+1]);
-        return result;
-        
-    }
-    
-    private static void dfs(int dep, int st, boolean[] number){
-        
-        if(dep == 5){
-            if(isPossible(number)) result++;
-            return;
-        }
-        
-        for(int i = st; i < arr.size(); i++){
-            int now = arr.get(i);
-            number[now] = true;
-            dfs(dep+1,i+1,number);
-            number[now] = false;
+            dfs(i+1,arr);
+            arr.remove(arr.size() - 1);
         }
         
     }
     
-    private static boolean isPossible(boolean[] number){
-        
-        // ArrayList<Integer> numberArr = new ArrayList<>();
-        // for(int i = 0; i < number.length; i++){
-        //     if(!number[i]) continue;
-        //     numberArr.add(i);
-        //     System.out.printf("%d ",i);
-        // }
-        // System.out.println();
+    private boolean isPossible(ArrayList<Integer> arr){
         
         for(int i = 0; i < q.length; i++){
+            int[] number = q[i];
             int cnt = 0;
-            for(int j = 0; j < q[i].length; j++){
-                int num = q[i][j];
-                if(number[num]) cnt++;
+            for(int j = 0 ; j < size; j++){
+                int num = number[j];
+                for(int k = 0; k < size; k++){
+                    if(num == arr.get(k)) cnt++;
+                }
             }
-            if(cnt != ans[i]) return false;
+            
+            if(ans[i] != cnt) return false;
+            
         }
         
         return true;
