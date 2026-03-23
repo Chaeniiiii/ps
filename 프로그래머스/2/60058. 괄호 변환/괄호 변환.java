8aa6064a1e737167ef8349b3926a305161a68299
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-03-23 15:49:09 +0900

    [level 2] Title: 괄호 변환, Time: 22.46 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
@@ -1,69 +1,62 @@
+import java.util.*;
+
 class Solution {
-
-    private static class Parentheses {
-        String u, v;
-        boolean correct;
-
-        Parentheses(String u, String v, boolean correct) {
-            this.u = u;
-            this.v = v;
-            this.correct = correct;
-        }
-    }
-
     public String solution(String p) {
         
-        if (p.isEmpty()) return "";
-
-        Parentheses pt = divP(p);
-
-        if (pt.correct) {
-            return pt.u + solution(pt.v);
-        } else {
-            return makeCorrect(pt);
-        }
+        return game(p);        
         
     }
-
-    private String makeCorrect(Parentheses pt) {
+    
+    private String game(String p){
         
-        StringBuilder sb = new StringBuilder();
-
-        sb.append("(");
-        sb.append(solution(pt.v));
-        sb.append(")");
-
-        String u = pt.u.substring(1, pt.u.length() - 1);
-        for (char c : u.toCharArray()) {
-            sb.append(c == '(' ? ')' : '(');
-        }
-
-        return sb.toString();
+        if(p.length() == 0) return "";
         
-    }
-
-    private Parentheses divP(String p) {
+        //u,v 분리
+        int lt = 0, rt = 0;
         
-        int balance = 0;
-        boolean correct = true;
-
-        for (int i = 0; i < p.length(); i++) {
-            
+        StringBuilder u = new StringBuilder(); 
+        StringBuilder v = new StringBuilder(); 
+        Deque<Character> deque = new ArrayDeque<>();        
+        
+        for(int i = 0; i < p.length(); i++){
             char c = p.charAt(i);
+            if(c == '('){
+                lt++;
+                deque.add(c);
+            }
+            else{
+                rt++;
+                deque.poll();
+            }
             
-            balance += (c == '(') ? 1 : -1;
-
-            if (balance < 0) correct = false;
-
-            if (balance == 0) {
-                String u = p.substring(0, i + 1);
-                String v = p.substring(i + 1);
-                return new Parentheses(u, v, correct);
+            if(lt == rt){
+                u = new StringBuilder(p.substring(0,i+1));
+                v = new StringBuilder(p.substring(i+1));
+                break;
+                
             }
             
         }
         
-        return null;
+        
+        //올바른 문자열인지 확인
+        if(deque.isEmpty()){
+           return u.toString() + game(v.toString()); 
+        }
+        else{
+            if(u.length() <= 2){
+                return '(' + game(v.toString()) + ')';
+            }
+            else{
+                StringBuilder temp = new StringBuilder();
+                for(int i = 1; i < u.length() - 1; i++){
+                    if(u.charAt(i) == '(') temp.append(')');
+                    else temp.append('(');
+                }
+                return '(' + game(v.toString()) + ')' + temp.toString();
+            }
+            
+        }
         
     }
 }
