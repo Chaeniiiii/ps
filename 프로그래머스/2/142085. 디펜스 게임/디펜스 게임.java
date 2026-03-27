7a907ef7ded280b6ad0a17e4b8de1c724fb25ac6
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-03-27 11:11:09 +0900

    [level 2] Title: 디펜스 게임, Time: 115.42 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -3,27 +3,25 @@ import java.util.*;
 class Solution {
     public int solution(int n, int k, int[] enemy) {
         
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b - a);
-        int cnt = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
         
-        for(int i = 0; i < enemy.length; i++){
+        int i = 0;
+        for(; i < enemy.length; i++){
             
-            n -= enemy[i];
-            pq.add(enemy[i]);
+            int enm = enemy[i];
+            n -=enm;
+            pq.add(enm);
             
-            if(n < 0){
-                if(k == 0) return i;
-                if(k > 0 && !pq.isEmpty()){
-                    k--;
-                    n += pq.poll();
-                }
+            if(k > 0 && n < 0 && !pq.isEmpty()){
+                k--;
+                n += pq.poll();
             }
             
-            cnt++;
+            if(n < 0) break;
             
-        }
+        } 
         
-        return cnt;
+        return i;
         
     }
 }
