2a350e9d1bfa929b2983e56492416de8c3a6b168
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-03-30 23:16:20 +0900

    [level 2] Title: 지게차와 크레인, Time: 57.49 ms, Memory: 94.6 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
@@ -2,44 +2,35 @@ import java.util.*;
 
 class Solution {
     
-    private static int n,m;
+    private int n,m,cnt;
+    private String[] storage;
+    private boolean[][] visited;
     
-    private static char[][] board;
-    private static boolean[][] visited;
-    
-    private static int[] dx = new int[]{-1,1,0,0};
-    private static int[] dy = new int[]{0,0,-1,1};
+    private int[] dx = new int[]{-1,1,0,0};
+    private int[] dy = new int[]{0,0,-1,1};
     
     public int solution(String[] storage, String[] requests) {
         
+        this.storage = storage;
+        
         n = storage.length;
         m = storage[0].length();
-        board = new char[n][m];
-        visited = new boolean[n][m];
+        cnt = n*m;
         
-        for(int i = 0; i < n; i++){
-            String str = storage[i];
-            for(int j = 0; j < m; j++){
-                board[i][j] = str.charAt(j);
-            }
-        }
+        visited = new boolean[n][m];
         
         for(int i = 0; i < requests.length; i++){
             
-            String now = requests[i];
-            if(now.length() == 1){
-                fork(now.charAt(0));
-            }
-            else{
-                crane(now);
-            }
+            String rq = requests[i];
+            int len = rq.length();
             
-        }
-        
-        int cnt = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(!visited[i][j]) cnt++;
+            char c = rq.charAt(0);
+            
+            if(len == 1){ //지게차
+                fork(c);
+            } 
+            else{ //크레인 
+                crane(c);
             }
         }
         
@@ -47,66 +38,65 @@ class Solution {
         
     }
     
-    private static void fork(char c){
-        
-        Deque<int[]> deque = new ArrayDeque<>();
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(board[i][j] == c && !visited[i][j]){
-                    if(isPossible(i,j)) deque.add(new int[]{i,j});
-                }
-            }
-        }
-        
-        while(!deque.isEmpty()){
-            
-            int[] now = deque.poll();
-            visited[now[0]][now[1]] = true;
-            
-        }
-        
-    }
-    
-    private static void crane(String now){
+    private void crane(char c){
         
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(board[i][j] == now.charAt(0)){
-                    visited[i][j] = true;
-                }
+                if(storage[i].charAt(j) != c || visited[i][j]) continue;
+                visited[i][j] = true;
+                cnt--;
             }
         }
         
     }
     
-    private static boolean isPossible(int sx, int sy){
+    private void fork(char c){
         
+        ArrayList<int[]> arr = new ArrayList<>();
+        
+        for(int x = 0; x < n; x++){
+            for(int y = 0; y < m; y++){
+                if(storage[x].charAt(y) == c && !visited[x][y] && isPossible(x,y,c)){
+                    arr.add(new int[]{x,y});
+                    cnt--;
+                }
+            }
+        }
+        
+        for(int[] pos : arr){
+            visited[pos[0]][pos[1]] = true;
+        }
+        
+    }
+    
+    private boolean isPossible(int x, int y, char c){
+        
+        if(x == 0 || y == 0 || x == n-1 || y == m - 1) return true;
         Deque<int[]> deque = new ArrayDeque<>();
-        deque.add(new int[]{sx,sy});
+        deque.add(new int[]{x,y});
         
-        boolean[][] nowV = new boolean[n][m];
+        boolean[][] nowVisited = new boolean[n][m];
+        nowVisited[x][y] = true;
         
         while(!deque.isEmpty()){
             
             int[] now = deque.poll();
-            int x = now[0];
-            int y = now[1];
-            nowV[x][y] = true;
             
-            for(int i = 0; i < 4; i++){
+            for(int k = 0; k < 4; k++){
+                int nx = now[0] + dx[k];
+                int ny = now[1] + dy[k];
                 
-                int mx = x + dx[i];
-                int my = y + dy[i];
+                if(nx < 0 || ny < 0 || nx >= n || ny >= m || !visited[nx][ny] || nowVisited[nx][ny]) continue;
+                if((nx == 0 || ny == 0 || nx == n - 1 || ny == m - 1) && visited[nx][ny]) return true;
                 
-                if(mx < 0 || my < 0 || mx >= n || my >= m) return true;
-                if(!nowV[mx][my] && visited[mx][my]) deque.add(new int[]{mx,my});
+                nowVisited[nx][ny] = true;
+                if(visited[nx][ny]) deque.add(new int[]{nx,ny});
                 
             }
             
         }
         
         return false;
-        
     }
     
 }
