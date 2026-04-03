8b265c936c8bbd5bfc8de8132c549e90445ecb02
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-04-03 12:43:56 +0900

    [Silver I] Title: 경로 찾기, Time: 156 ms, Memory: 16716 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] graph = new int[n][n];
+        StringTokenizer st;
+
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < n; j++){
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+    
+        for(int k = 0; k < n; k++){
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    if(graph[i][k] == 1 && graph[k][j] == 1) graph[i][j] = 1;
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                sb.append(graph[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb.toString());
+
+    }
+
+}
