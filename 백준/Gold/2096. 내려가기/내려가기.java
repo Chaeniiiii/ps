84646646f13b31cce6b201181c84abbdab53f12e
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-04-05 00:01:36 +0900

    [Gold V] Title: 내려가기, Time: 336 ms, Memory: 47820 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -8,55 +8,48 @@ public class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        int[][] board = new int[n][3];
         StringTokenizer st;
+
+        int[] minR = new int[3];
+        int[] maxR = new int[3];
+
         for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
+            int c1 = Integer.parseInt(st.nextToken());
+            int c2 = Integer.parseInt(st.nextToken());
+            int c3 = Integer.parseInt(st.nextToken());
+
+            int min1 = minR[0] , min2 = minR[1], min3 = minR[2];
+            int max1 = maxR[0] , max2 = maxR[1], max3 = maxR[2];
+
             for(int j = 0; j < 3; j++){
-                board[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        int[][] dp = new int[n][3];
-        dp[0] = board[0].clone();
-
-        //최대 점수 구하기 
-        for(int i = 1; i < n; i++){
-            for(int j = 0; j < 3; j++){
-                if(j == 0){
-                    dp[i][j] = Math.max(dp[i-1][j],dp[i-1][j+1]) + board[i][j];
+                
+                if(i == 0){
+                    minR[0] = maxR[0] = c1;
+                    minR[1] = maxR[1] = c2;
+                    minR[2] = maxR[2] = c3;
                 }
-                if(j == 1){
-                    dp[i][j] = Math.max(dp[i-1][j],Math.max(dp[i-1][j-1], dp[i-1][j+1])) + board[i][j];
-                }
-                if(j == 2){
-                    dp[i][j] = Math.max(dp[i-1][j],dp[i-1][j-1]) + board[i][j]; 
-                }
-            }
-        }
-
-        int rMax = Math.max(Math.max(dp[n-1][0],dp[n-1][1]),dp[n-1][2]);
-
-        dp = new int[n][3];
-        dp[0] = board[0].clone();
-        //최소 점수 구하기 
-        for(int i = 1; i < n; i++){
-            for(int j = 0; j < 3; j++){
-                if(j == 0){
-                    dp[i][j] = Math.min(dp[i-1][j],dp[i-1][j+1]) + board[i][j];
-                }
-                if(j == 1){
-                    dp[i][j] = Math.min(dp[i-1][j],Math.min(dp[i-1][j-1], dp[i-1][j+1])) + board[i][j];
-                }
-                if(j == 2){
-                    dp[i][j] = Math.min(dp[i-1][j],dp[i-1][j-1]) + board[i][j]; 
+                else{
+                    if(j == 0){
+                        minR[j] = c1 + Math.min(min1,min2);
+                        maxR[j] = c1 + Math.max(max1,max2);
+                    }
+                    if(j == 1){
+                        minR[j] = c2 + Math.min(min3,Math.min(min1,min2));
+                        maxR[j] = c2 + Math.max(max3,Math.max(max1,max2));
+                    }
+                    if(j == 2){
+                        minR[j] = c3 + Math.min(min3,min2);
+                        maxR[j] = c3 + Math.max(max3,max2);
+                    }
                 }
             }
         }
 
-        int rMin = Math.min(Math.min(dp[n-1][0],dp[n-1][1]),dp[n-1][2]);
-        System.out.printf("%d %d",rMax,rMin);
+        int minResult = Math.min(minR[0], Math.min(minR[1],minR[2]));
+        int maxResult = Math.max(maxR[0], Math.max(maxR[1],maxR[2]));
 
+        System.out.printf("%d %d ",maxResult,minResult);
 
     }
 
