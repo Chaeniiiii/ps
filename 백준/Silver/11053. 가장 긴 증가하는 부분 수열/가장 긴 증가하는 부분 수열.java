a5997cf2e7610ba4b2e51a764f12a456fb41d752
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-04-05 02:12:42 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 128 ms, Memory: 14516 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -3,21 +3,26 @@ import java.io.*;
 
 public class Main {
 
-    private static int[] dp;
+    private static int[] dp, number;
     public static void main(String[] args) throws Exception{
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] number = new int[n];
+        number = new int[n];
         for(int i = 0; i < n; i++){
             number[i] = Integer.parseInt(st.nextToken());
         }
 
         dp = new int[n];
         for(int i = 0; i < n; i++){ 
-            lis(i,number);
+            dp[i] = 1;
+            for(int j = 0; j < n; j++){
+                if(number[j] < number[i]){
+                    dp[i] = Math.max(dp[i],dp[j] + 1);
+                }
+            }
         }
 
         int result = 1;
@@ -29,18 +34,4 @@ public class Main {
 
     }
 
-    private static int lis(int n, int[] number){
-
-        if(dp[n] != 0) return dp[n];
-        dp[n] = 1;
-
-        for(int i = n - 1; i >= 0; i--){
-            if(number[i] < number[n]){
-                dp[n] = Math.max(dp[n], lis(i,number) + 1);
-            }
-        }
-
-        return dp[n];
-
-    }
 }
