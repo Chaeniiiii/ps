11ac4e57881f789839334038852dff897c4ba2dc
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-04-07 16:24:16 +0900

    [Gold IV] Title: 구슬 찾기, Time: 164 ms, Memory: 15744 KB -BaekjoonHub

--- 백준/Gold/2617. 구슬 찾기/구슬 찾기.java
+++ 백준/Gold/2617. 구슬 찾기/구슬 찾기.java
@@ -11,24 +11,22 @@ public class Main {
         int m = Integer.parseInt(st.nextToken());
 
         int[][] graph = new int[n+1][n+1];
-
         for(int i = 0; i < m; i++){
             st = new StringTokenizer(br.readLine());
+            
             int v = Integer.parseInt(st.nextToken());
             int u = Integer.parseInt(st.nextToken());
 
-            graph[u][v] = 1; //가벼운 것
-            graph[v][u] = -1; //무거운 것
+            graph[v][u] = 1;
+            graph[u][v] = -1;
 
         }
 
         for(int k = 1; k <= n; k++){
             for(int i = 1; i <= n; i++){
                 for(int j = 1; j <= n; j++){
-                    //i가 k보다 가볍고, k가 j보다 가볍다면, i는 j보다 가벼움 ! 
                     if(graph[i][k] == 1 && graph[k][j] == 1) graph[i][j] = 1;
-                    //i가 k보다 무겁고, k가 j보다 무겁다면, i는 j보다 무겁움 !
-                    if(graph[i][k] == -1 && graph[k][j] == -1) graph[i][j] = -1;
+                    else if(graph[i][k] == -1 && graph[k][j] == -1) graph[i][j] = -1;
                 }
             }
         }
@@ -42,10 +40,11 @@ public class Main {
                 if(graph[i][j] == 1) lg++;
                 else if(graph[i][j] == -1) hv++;
             }
-            if(hv > mid || lg > mid) cnt++; 
+            if(hv > mid || lg > mid) cnt++;
         }
 
         System.out.println(cnt);
+        
 
     }
 }
