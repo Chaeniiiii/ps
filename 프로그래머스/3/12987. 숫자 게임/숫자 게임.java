db84c12120803a68605c6f1fd901cc5c70f14f5c
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-04-26 17:18:25 +0900

    [level 3] Title: 숫자 게임, Time: 136.24 ms, Memory: 69.4 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -2,21 +2,27 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] A, int[] B) {
-        
+         
+        int cnt = 0;
         Arrays.sort(A);
         Arrays.sort(B);
         
-        int aIdx = 0, bIdx = 0, result = 0;
-        
-        while(aIdx < A.length && bIdx < B.length){
-            if(A[aIdx] < B[bIdx]){
-                aIdx++;
-                result++;
-            }
-            bIdx++;
+        Deque<Integer> deque = new ArrayDeque<>();
+        for(int i = 0; i < B.length; i++){
+            deque.add(B[i]);
         }
         
-        return result;
+        for(int i = A.length - 1; i >= 0; i--){
+            if(A[i] >= deque.peekLast()){
+                deque.poll();
+            }
+            else{
+                deque.pollLast();
+                cnt++;
+            }
+        }
+        
+        return cnt;
         
     }
 }
