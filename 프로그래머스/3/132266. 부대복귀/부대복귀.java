774e0a56d9fc110c5f7688923fed9cd2023a106f
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-05-03 16:58:26 +0900

    [level 3] Title: 부대복귀, Time: 202.74 ms, Memory: 193 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -2,68 +2,69 @@ import java.util.*;
 
 class Solution {
     
-    private static class Node {
-        
-        int val;
-        ArrayList<Node> children = new ArrayList<>();
-        
-        private Node(int val){
-            this.val = val;
-        }
-        
-    }
+    private List<Integer>[] graph;
     
     public int[] solution(int n, int[][] roads, int[] sources, int destination) {
         
-        Node [] nodes = new Node [n+1];
-        for(int i = 1; i<=n; i++) nodes[i] = new Node(i);
-            
-        for(int [] road : roads){
-            nodes[road[0]].children.add(nodes[road[1]]);
-            nodes[road[1]].children.add(nodes[road[0]]);
-        }                
+        //1. 그래프 만들기
+        graph = new ArrayList[n+1];
+        for(int i = 1; i <= n; i++){
+            graph[i] = new ArrayList<>();
+        }
         
-        int [] result = new int[sources.length];
-        for(int i = 0; i<sources.length; i++){
-            result[i] = bfs(n, nodes[sources[i]],nodes[destination]);
-        }        
+        for(int[] r : roads){
+            int v = r[0];
+            int u = r[1];
+            graph[v].add(u);
+            graph[u].add(v);
+        }
+        
+        //2. 다익스트라 실행
+        int[] dist = dijkstra(n,destination);
+        
+        int[] result = new int[sources.length];
+        //3.최단시간 구하기
+        for(int i = 0; i < sources.length; i++){
+            if(dist[sources[i]] == Integer.MAX_VALUE) result[i] = -1;
+            else result[i] = dist[sources[i]];
+        }
         
         return result;
         
     }
     
-    private static int bfs(int n, Node st,Node en){
+    private int[] dijkstra(int n, int dt){
         
-        Deque<Node> deque = new ArrayDeque<>();
-        deque.add(st);
+        int[] dist = new int[n+1];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[dt] = 0;
         
-        boolean [] visited = new boolean[n+1];
-        visited[st.val] = true;
+        boolean[] visited = new boolean[n+1];
         
-        int cnt = 0;
-        while(!deque.isEmpty()){
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
+        pq.add(new int[]{dt,0});
+        
+        while(!pq.isEmpty()){
             
-            int size = deque.size();
+            int[] now = pq.poll();
+            int node = now[0];
+            int d = now[1];
             
-            for(int i = 0; i<size; i++){
-                
-                Node now = deque.poll();
-                if(now.val == en.val) return cnt;
-                
-                for(Node nxt : now.children){
-                    
-                    if(visited[nxt.val]) continue;
-                    deque.add(nxt);
-                    visited[nxt.val] = true;
-                    
+            visited[node] = true;
+            
+            for(int child : graph[node]){
+                if(visited[child]) continue;
+                if(d + 1 < dist[child]){
+                    dist[child] = d + 1;
+                    pq.add(new int[]{child,dist[child]});
                 }
             }
             
-            cnt ++;
-            
         }
         
-        return -1;
+        return dist;
+        
         
     }
 }
