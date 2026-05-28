c198c501ddf7f9420333a3f763f23cd8196b6f95
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-05-28 14:53:10 +0900

    [level 3] Title: 정수 삼각형, Time: 8.76 ms, Memory: 66.6 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -3,11 +3,11 @@ import java.util.*;
 class Solution {
     public int solution(int[][] triangle) {
         
-        int r = triangle.length;
-        int[][] dp = new int[r][r];
+        int n = triangle.length;
+        int[][] dp = new int[n][n];
         dp[0][0] = triangle[0][0];
         
-        for(int i = 0; i < r - 1; i++){
+        for(int i = 0; i < n - 1; i++){
             for(int j = 0; j < triangle[i].length; j++){
                 dp[i+1][j] = Math.max(dp[i+1][j],dp[i][j] + triangle[i+1][j]);
                 dp[i+1][j+1] = Math.max(dp[i+1][j+1],dp[i][j] + triangle[i+1][j+1]);
@@ -15,12 +15,11 @@ class Solution {
         }
         
         int result = 0;
-        for(int i = 0; i < triangle[r-1].length; i++){
-            result = Math.max(result,dp[r-1][i]);
+        for(int i = 0; i < n; i++){
+            result = Math.max(result,dp[n-1][i]);
         }
         
         return result;
-        
         
     }
 }
