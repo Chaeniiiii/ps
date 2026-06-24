23d8b2ef201d0badd4363f6da3e8da067c220d0a
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-06-24 20:12:57 +0900

    Time: 0 ms (100%), Space: 42.2 MB (34.92%) - LeetHub

--- LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
@@ -3,15 +3,18 @@ import java.util.*;
 class Solution {
     public int climbStairs(int n) {
         
-        int[] dp = new int[n+1];
+        if(n == 1) return 1;
+        if(n == 2) return 2;
 
-        dp[0] = dp[1] = 1;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
 
-        for(int i = 2; i <= n; i++){
+        for(int i = 2; i < n; i++){
             dp[i] = dp[i-1] + dp[i-2];
         }
 
-        return dp[n];
+        return dp[n-1];
 
     }
 }
