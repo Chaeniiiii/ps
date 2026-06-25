ee334e3865980b29117500b26adf8232cbfb3356
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-06-25 18:21:56 +0900

    Time: 3 ms (73.82%), Space: 46.3 MB (86.16%) - LeetHub

--- LeetCode/Medium/0740-delete-and-earn/0740-delete-and-earn.java
+++ LeetCode/Medium/0740-delete-and-earn/0740-delete-and-earn.java
@@ -15,7 +15,7 @@ class Solution {
         dp[0] = arr[0];
         dp[1] = arr[1];
 
-        for(int i = 2; i < dp.length; i++){
+        for(int i = 2; i <= max; i++){
             dp[i] = Math.max(dp[i-1],dp[i-2] + arr[i]);
         }
 
