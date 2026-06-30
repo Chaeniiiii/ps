e9648bf4b734cf1d47dc65c82777966d16b76b75
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-07-01 00:08:48 +0900

    Time: 9 ms (33.74%), Space: 71.1 MB (26.5%) - LeetHub

--- LeetCode/Medium/0221-maximal-square/0221-maximal-square.java
+++ LeetCode/Medium/0221-maximal-square/0221-maximal-square.java
@@ -1,24 +1,25 @@
-import java.util.*;
-
 class Solution {
     public int maximalSquare(char[][] matrix) {
         
         int n = matrix.length;
         int m = matrix[0].length;
 
-        int[][] dp = new int[n+1][m+1];
-        int result = 0;
+        if(n == 1 && m == 1) return matrix[0][0] - '0';
 
+        int[][] dp = new int[n+1][m+1];
+        int max = 0;
+        
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
                 if(matrix[i-1][j-1] == '1'){
-                    dp[i][j] = Math.min(Math.min(dp[i-1][j-1],dp[i][j-1]),dp[i-1][j])+1;
-                    result = Math.max(result,dp[i][j]);
+                    dp[i][j] = Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1])) + 1;
                 }
+                max = Math.max(dp[i][j],max);
             }
-        }
-        
-        return result*result;
+
+        }        
+
+        return max * max;
 
     }
 }
