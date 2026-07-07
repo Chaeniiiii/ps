810fdd51b7ba16aec64ae9b53a50cc4a439d6eb6
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-07-07 16:16:57 +0900

    Time: 34 ms (72.71%), Space: 64.2 MB (21.92%) - LeetHub

--- LeetCode/Medium/0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ LeetCode/Medium/0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -5,26 +5,20 @@ class Solution {
 
         int len = s.length();
         int[][] dp = new int[len][len];   
-        for(int i = 0; i < len; i++){
-            dp[i][i] = 1;
-        }
-        
-        int result = 1;    
 
         for(int i = len - 1; i >= 0; i--){
+            dp[i][i] = 1;
             for(int j = i + 1; j < len; j++){
-                if(i == j) dp[i][j] = 1;
                 if(s.charAt(i) == s.charAt(j)){
                     dp[i][j] = 2 + dp[i+1][j-1];
                 }
                 else{
                     dp[i][j] = Math.max(dp[i+1][j],dp[i][j-1]);
                 }
-                result = Math.max(result,dp[i][j]);
             }
         }
 
-        return result;
+        return dp[0][len - 1];
 
     }
 }
