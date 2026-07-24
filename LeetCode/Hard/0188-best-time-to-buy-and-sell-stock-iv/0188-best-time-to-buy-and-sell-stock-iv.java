e9ddda73cf808d2dce134ac22cf6d345ab1524e9
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-07-24 21:18:47 +0900

    Time: 1 ms (99.84%), Space: 43.3 MB (84.17%) - LeetHub

--- LeetCode/Hard/0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ LeetCode/Hard/0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,19 +1,19 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
-        
-        int[][][] dp = new int[k+1][prices.length][2];
-        for(int i = 1; i <= k; i++){
-            dp[i][0][0] = -prices[0];
-        }
+
+        int[] buy = new int[k+1];        
+        int[] sell = new int[k+1];  
+
+        Arrays.fill(buy,-prices[0]);
 
         for(int i = 1; i < prices.length; i++){
             for(int j = 1; j <= k; j++){
-                dp[j][i][0] = Math.max(dp[j-1][i-1][1] - prices[i], dp[j][i-1][0]);
-                dp[j][i][1] = Math.max(dp[j][i-1][0] + prices[i],dp[j][i-1][1]);
+                buy[j] = Math.max(buy[j],sell[j-1] - prices[i]);
+                sell[j] = Math.max(sell[j],buy[j] + prices[i]);
             }
         }
 
-        return dp[k][prices.length-1][1];
+        return sell[k];
 
     }
 }
