2357c98de99ce10d7923529d339565e26d622bcd
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-07-24 19:16:48 +0900

    Time: 25 ms (29.56%), Space: 54 MB (36.03%) - LeetHub

--- LeetCode/Medium/1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ LeetCode/Medium/1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -3,21 +3,25 @@ import java.util.*;
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         
-        int[][] dp = new int[text1.length()+1][text2.length()+1];
+        int n = text1.length();
+        int m = text2.length();
 
-        for(int i = 1; i < text1.length()+1; i++){
-            for(int j = 1; j < text2.length()+1; j++){
+        int[][] dp = new int[n+1][m+1];
+        int result = 0;
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
                 if(text1.charAt(i-1) == text2.charAt(j-1)){
                     dp[i][j] = dp[i-1][j-1] + 1;
                 }
                 else{
-                    dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]);
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
                 }
+                result = Math.max(result,dp[i][j]);
             }
-
         }
 
-        return dp[text1.length()][text2.length()];
+        return result;
 
     }
 }
