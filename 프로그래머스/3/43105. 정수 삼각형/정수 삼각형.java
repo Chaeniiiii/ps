db4cd9cbfdca5a866f0556110c8912aaf0e8a641
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-08-03 18:39:50 +0900

    [level 3] Title: 정수 삼각형, Time: 7.67 ms, Memory: 65.4 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -8,9 +8,9 @@ class Solution {
         dp[0][0] = triangle[0][0];
         
         for(int i = 0; i < n - 1; i++){
-            for(int j = 0; j < triangle[i].length; j++){
-                dp[i+1][j] = Math.max(dp[i+1][j],dp[i][j] + triangle[i+1][j]);
-                dp[i+1][j+1] = Math.max(dp[i+1][j+1],dp[i][j] + triangle[i+1][j+1]);
+            for(int j = 0; j < i+1; j++){
+                dp[i+1][j] = Math.max(dp[i+1][j], triangle[i+1][j] + dp[i][j]);
+                dp[i+1][j+1] = Math.max(dp[i+1][j+1], triangle[i+1][j+1] + dp[i][j]);
             }
         }
         
