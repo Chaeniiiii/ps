82e5a2838b16dcc42840001383e09fe14617ee91
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-08-18 18:10:19 +0900

    [level 3] Title: 등굣길, Time: 0.64 ms, Memory: 57.3 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,23 +1,22 @@
 import java.util.*;
 
 class Solution {
-    
-    private final int MOD = 1_000_000_007;
-    
     public int solution(int m, int n, int[][] puddles) {
         
+        final int MOD = 1_000_000_007;
+        int[][] map = new int[m][n];
         int[][] dp = new int[m][n];
-        boolean[][] pd = new boolean[m][n];
+        dp[0][0] = 1;
         
-        for(int [] puddle : puddles){
-            pd[puddle[0]-1][puddle[1]-1] = true;
+        for(int i = 0; i < puddles.length; i++){
+            int[] pos = puddles[i];
+            map[pos[0]-1][pos[1]-1] = 1;
         }
         
-        dp[0][0] = 1;
-        for(int i = 0; i < m ; i++){
+        for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(i+1 < m && !pd[i+1][j]) dp[i+1][j] += dp[i][j] % MOD;
-                if(j+1 < n && !pd[i][j+1]) dp[i][j+1] += dp[i][j] % MOD;
+                if(i - 1 >= 0 && map[i-1][j] == 0) dp[i][j] += dp[i-1][j] % MOD ;
+                if(j - 1 >= 0 && map[i][j-1] == 0) dp[i][j] += dp[i][j-1] % MOD ;
             }
         }
         
