be9d99152495e20e88572e2ca15038dc2f44481e
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-08-18 23:52:15 +0900

    [level 3] Title: 스티커 모으기(2), Time: 4.11 ms, Memory: 61 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -1,10 +1,27 @@
+import java.util.*;
+
 class Solution {
     public int solution(int sticker[]) {
-        int answer = 0;
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
-        return answer;
+        
+        int len = sticker.length;
+        if(len == 1) return sticker[0];
+        if(len == 2) return Math.max(sticker[0],sticker[1]);
+        
+        int[] dp = new int[len - 1];
+        dp[0] = dp[1] = sticker[0];
+        
+        for(int i = 2; i < dp.length; i++){
+            dp[i] = Math.max(dp[i-1],dp[i-2] + sticker[i]);
+        }
+        
+        int[] dp2 = new int[len];
+        dp2[1] = sticker[1];
+        
+        for(int i = 2; i < dp2.length; i++){
+            dp2[i] = Math.max(dp2[i-1],dp2[i-2] + sticker[i]);
+        }
+        
+        return Math.max(dp[dp.length - 1],dp2[dp2.length - 1]);
+        
     }
 }
