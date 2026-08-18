a4c0216c780f399e4890fac823d2a6fdb47ef981
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-08-18 18:52:27 +0900

    [level 3] Title: 단속카메라, Time: 5.04 ms, Memory: 58.9 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -3,14 +3,14 @@ import java.util.*;
 class Solution {
     public int solution(int[][] routes) {
         
-        Arrays.sort(routes, (a,b) -> a[1] - b[1]);
-        int end = Integer.MIN_VALUE;
-        int cnt = 0;
-        
-        for(int i = 0; i < routes.length; i++){
+        Arrays.sort(routes,(a,b) -> a[1] - b[1]);
+
+        int cnt = 1;
+        int end = routes[0][1];
+        for(int i = 1; i < routes.length; i++){
             if(routes[i][0] > end){
+                end = Math.max(end,routes[i][1]);
                 cnt++;
-                end = routes[i][1];
             }
         }
         
