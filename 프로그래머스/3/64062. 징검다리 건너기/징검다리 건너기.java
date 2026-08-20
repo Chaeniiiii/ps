a50aa38dce2445bc9ab4e5be83f3552bfb0576dd
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-08-20 17:38:28 +0900

    [level 3] Title: 징검다리 건너기, Time: 20.01 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,46 +1,51 @@
 import java.util.*;
 
 class Solution {
-    
-    private int[] stones;
+
     private int k;
+    private int[] stones;
     
     public int solution(int[] stones, int k) {
-
+        
         this.k = k;
         this.stones = stones;
-        int result = 0;
-        int lt = 1, rt = 200_000_000;
         
+        int lt = 1 , rt = 200_000_000 * 2;
+        int result = 0;
         while(lt <= rt){
             
             int mid = (lt + rt) / 2;
             
             if(isPossible(mid)){
+                result = Math.max(mid,result);
                 lt = mid + 1;
-                result = Math.max(result,mid);
             }
             else{
                 rt = mid - 1;
             }
-            
         }
         
         return result;
         
     }
     
-    private boolean isPossible(int p){
+    private boolean isPossible(int cnt){
         
-        int pass = 0;
+        int skip = 0;
+        
         for(int i = 0; i < stones.length; i++){
-            if(stones[i] < p) pass++;
-            else pass = 0;
-            if(pass == k) return false;
+            if(stones[i] - cnt < 0){
+                skip++;
+            }
+            else{
+                skip = 0;
+            }
+            
+            if(skip == k) return false;
         }
         
         return true;
         
+        
     }
-    
 }
