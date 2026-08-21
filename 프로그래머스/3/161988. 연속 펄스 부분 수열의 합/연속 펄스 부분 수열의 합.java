7acab6719183d66af7f70f31f1fac66886199b4a
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-08-21 17:47:23 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 11.56 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -3,38 +3,25 @@ import java.util.*;
 class Solution {
     public long solution(int[] sequence) {
         
-        int len = sequence.length;
-        
-        long [] a = new long [len];
-        long [] b = new long [len];
-        
-        int [] purse = new int[len];
-        Arrays.fill(purse,1);
-        
-        for(int i = 0; i<len; i+=2) purse[i]*=-1;
-            
-        for(int i = 0; i<len; i++){
-            a[i] = sequence[i] * purse[i];
-            b[i] = sequence[i] * purse[i]*-1;
-        }
+        boolean type = true;
         
         long result = 0;
-        result = Math.max(calc(a,len),Math.max(calc(b,len),result));
+        long result1 = 0;
+        long result2 = 0;
+        
+        for(int num : sequence){
+            
+            result1 += type ? num : - num;
+            result2 += type ? -num : num;
+            
+            result1 = Math.max(result1,0);
+            result2 = Math.max(result2,0);
+            
+            result = Math.max(result,Math.max(result1,result2));
+            type = !type;
+        }
         
         return result;
         
     }
-    
-    private static long calc(long [] sqn, int len){
-        
-        long cnt = sqn[0];
-        for(int i = 1; i<len; i ++){
-            sqn[i] = Math.max(sqn[i],sqn[i-1]+sqn[i]);
-            cnt = Math.max(sqn[i],cnt);
-        }
-        
-        return cnt;
-        
-    }
-    
 }
