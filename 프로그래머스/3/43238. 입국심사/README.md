8526612d12c8efd101ebb8cb38109b1450262168
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-08-21 23:54:26 +0900

    [level 3] Title: 입국심사, Time: 33.50 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
 
 ### 성능 요약
 
-메모리: 84.7 MB, 시간: 32.25 ms
+메모리: 84.7 MB, 시간: 33.50 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 24일 19:25:26
+2026년 08월 21일 23:54:23
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,23 +1,38 @@
-import java.util.Arrays;
+import java.util.*;
+
 class Solution {
     public long solution(int n, int[] times) {
-        long answer = 0;
-        Arrays.sort(times);
-        long left = 0;
-        long right = times[times.length-1] * (long)n; //모든 사람이 가장 느리게 심사받음
         
-        while(left <= right) {
-            long mid = (left + right) / 2;
-            long complete = 0;
-            for (int i = 0; i < times.length; i++)
-                complete += mid / times[i];
-            if (complete < n) // 해당 시간에는 모든 사람이 검사받을 수 없다.
-                left = mid + 1;
-            else {
-                right = mid - 1;
-                answer = mid; // 모두 검사받았으나, 더 최솟값이 있을 수 있다.
+        Arrays.sort(times);
+        long lt = 0, rt = times[times.length - 1] * (long)n;
+        long result = 0;
+        
+        while(lt <= rt){
+            
+            long mid = (lt + rt) / 2;
+            if(test(mid,n,times)){
+                rt = mid - 1;
+                result = mid;
             }
-        }  
-        return answer;
+            else{
+                lt = mid + 1;
+            }
+            
+        }
+        
+        return result;
+        
     }
+    
+    private boolean test(long time,int n, int[] times){
+        
+        long cnt = 0;
+        for(int i = 0; i < times.length; i++){
+            cnt += time / times[i];
+        }
+        
+        return cnt >= n ? true : false;
+        
+    }
+    
 }
