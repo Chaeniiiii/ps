0527a5929f66bd0cb942cb00dc4ab86b9d9c61fd
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-09-19 17:50:12 +0900

    [level 3] Title: 카테고리 별 도서 판매량 집계하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
+++ 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
@@ -1,5 +1,5 @@
-select b.category, sum(sales) as total_sales
-from book_sales bs join book b on bs.book_id = b.book_id
-where bs.sales_date like "%2022-01%"
+select category, sum(sales) as "total_sales"
+from book b join book_sales bs on b.book_id = bs.book_id
+where bs.sales_date between "2022-01-01" and "2022-01-31"
 group by b.category
-order by b.category;
+order by category;
