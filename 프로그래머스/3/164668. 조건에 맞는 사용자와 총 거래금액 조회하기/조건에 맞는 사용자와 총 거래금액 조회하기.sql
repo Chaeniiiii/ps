485119d7d9d5119199f9eeccf68c8c422b1eb88c
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-09-20 17:11:51 +0900

    [level 3] Title: 조건에 맞는 사용자와 총 거래금액 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
+++ 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
@@ -1,7 +1,6 @@
-select ugu.user_id , ugu.nickname, sum(ugb.PRICE) as price
-from USED_GOODS_BOARD ugb join USED_GOODS_USER ugu on ugb.WRITER_ID = ugu.user_id
-where ugb.status = "DONE"
-group by ugu.user_id
-having sum(ugb.PRICE) >= 700000
-order by price;
-
+select ugu.user_id, ugu.nickname, sum(ugb.price) as total_sales
+from USED_GOODS_BOARD ugb join USED_GOODS_USER ugu on ugb.writer_id = ugu.user_id
+where status = "DONE" 
+group by ugb.writer_id
+having total_sales >= 700000
+order by total_sales;
