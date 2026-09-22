f556a5f4ba236868dfea6f981961f42eab893afb
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-09-23 00:45:05 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,10 +1,3 @@
-# select ao.animal_id, ao.name
-# from ANIMAL_OUTS ao
-# where animal_id in (
-    
-# )
-# limit 2;
-
 select ai.animal_id, ao.name
 from animal_ins ai join ANIMAL_OUTS ao on ai.animal_id = ao.animal_id
 order by (ao.datetime - ai.datetime) desc
