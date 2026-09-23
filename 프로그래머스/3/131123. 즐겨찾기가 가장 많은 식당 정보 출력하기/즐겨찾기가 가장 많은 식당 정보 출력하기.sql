dda9b0f024f356639478b44899619361b894e9fb
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-09-23 18:19:29 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,9 +1,12 @@
-select ri.food_type, ri.rest_id, ri.rest_name, newRi.fav
-from rest_info ri join 
-(
-    select food_type, max(favorites) as fav
+with fv_t as (
+    select 
+        food_type,
+        max(favorites) over(partition by food_type) as "favorites"
     from rest_info
-    group by food_type
-    order by food_type desc
-) newRi on ri.food_type = newRi.food_type and ri.favorites = newRi.fav
+)
+
+select ri.food_type, ri.rest_id, ri.rest_name, ri.favorites
+from rest_info ri join fv_t on ri.food_type = fv_t.food_type
+where ri.food_type = fv_t.food_type and ri.favorites = fv_t.favorites
+group by ri.food_type
 order by ri.food_type desc;
