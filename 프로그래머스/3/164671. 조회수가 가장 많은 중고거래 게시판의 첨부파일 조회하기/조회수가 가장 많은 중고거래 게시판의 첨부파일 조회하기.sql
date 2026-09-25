d7e5f88313dffe85abd7a25933633a09530da281
Chaeeun Kang <2020112112@dgu.ac.kr>
2026-09-25 22:08:57 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,14 +1,14 @@
-select concat('/home/grep/src/',
-              ugb.BOARD_ID,
-              '/',
-              ugf.FILE_ID,
-              ugf.FILE_NAME,
-              ugf.FILE_EXT) as 'file_path'
-from USED_GOODS_BOARD ugb 
-    join USED_GOODS_FILE ugf 
-    on ugb.BOARD_ID = ugf.BOARD_ID
-WHERE ugb.VIEWS = (
-    SELECT MAX(VIEWS)
-    FROM USED_GOODS_BOARD
+with view_cnt as (
+    select bgb.board_id
+    from USED_GOODS_BOARD bgb
+    order by bgb.views desc
+    limit 1
 )
-order by file_id desc;
+
+select concat('/home/grep/src/',ugf.board_id,'/',ugf.file_id,ugf.file_name,ugf.file_ext) as 'FILE_PATH'
+from USED_GOODS_FILE ugf 
+where ugf.board_id in (
+    select board_id
+    from view_cnt
+)
+order by ugf.file_id desc;
